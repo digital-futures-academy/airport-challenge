1cd91c05ecc7bf174363c2d9b48e6f4f2066863c
namcap1 <namcap1@hotmail.co.uk>
2021-03-19 12:00:24 +0000

    linted my spec

--- spec/airport.spec.js
+++ spec/airport.spec.js
@@ -1,6 +1,5 @@
 const test = require('../test_framwork');
 const Airport = require('../src/airport');
-const { assertEquals } = require('../test_framwork');
 
 const fullmessage = 'Airport is currently full';
 const notheremessage = 'This plane if not currently here';
@@ -16,7 +15,7 @@ test.it('Checking the array is same length', function(){
     let result = airport.land('my plane');
 
     test.assertEquals(result.length, 1);
-})
+});
 
 test.it('Checking it is adding the correct plane', function(){
     let airport = new Airport();
@@ -25,14 +24,14 @@ test.it('Checking it is adding the correct plane', function(){
     let result = airport.land('my plane');
 
     test.assertEquals(result.includes('my plane'), true);
-})
+});
 
 test.it('Checking can\'t land already landed plane', function(){
     let airport = new Airport();
-    airport.land('my plane')
+    airport.land('my plane');
     let result = airport.land('my plane');
     test.assertEquals(result, alreadylanded);
-})
+});
 
 console.log('Check if airport is full');
 
@@ -42,7 +41,7 @@ test.it('Checking I can\'t land when airport is full', function(){
     airport.land('your plane');
 
     test.assertEquals(airport.land('his plane'), fullmessage);
-})
+});
 
 console.log('Take off function');
 
@@ -51,13 +50,13 @@ test.it('Checking if plane is taking off and is no longer there', function(){
     airport.land('my plane');
     let result = airport.takeoff('my plane');
     test.assertEquals(result.includes('my plane'), false);
-})
+});
 
 test.it('Checking that can\'t take off a plane that isn\'t landed at airport', function(){
     let airport = new Airport();
     let result = airport.takeoff('my plane');
     test.assertEquals(result, notheremessage);
-})
+});
 
 console.log('Stormy Weather');
 
@@ -67,13 +66,13 @@ test.it('Checking can\'t take off when bad weather', function(){
     airport.isStormy(true);
     let result = airport.takeoff('my plane');
     test.assertEquals(result, badweathertakeoffmessage);
-})
+});
 
 test.it('Checking can\'t land when bad weather', function(){
     let airport = new Airport(undefined, true);
     let result = airport.land('my plane');
     test.assertEquals(result, badweatherlandmessage);
-})
+});
 
 console.log('Amount of plane function');
 
@@ -83,7 +82,7 @@ test.it('Check current amount of planes at airport', function(){
     airport.land('your plane');
     let result = airport.currentAmountOfPlanes();
     test.assertEquals(result, 2);
-})
+});
 
 test.it('Checking that only landed planes are displayed', function(){
     let airport = new Airport();
@@ -92,7 +91,7 @@ test.it('Checking that only landed planes are displayed', function(){
     airport.land('my plane');
     let result2 = airport.current_planes;
     test.assertEquals(result2.includes('my plane'), true);
-})
+});
 
 console.log('Mutiple airports');
 
@@ -103,5 +102,5 @@ test.it('Mutiple airports and checking planes are in correct airport', function(
     airport2.land('your plane');
     let result = airport2.current_planes;
     test.assertEquals(result.includes('my plane'), false);
-})
+});
 test.print();
