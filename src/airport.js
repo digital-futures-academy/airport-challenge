75b6e24d616870ee697d04b8a77601e77d07b03a
Lantana Ali <lantanaali20@gmail.com>
2021-03-21 22:30:50 +0000

    Source code for third test case

--- src/airport.js
+++ src/airport.js
@@ -1,13 +1,24 @@
 class Airport {
-  constructor() {
+  constructor(capacity = 10) {
+    this.capacity = capacity;
     this.planeHangar = [];
   }
   landPlane(plane){
+    if(this.fullAirport){
+      return 'cannot land plane - aiport is full';
+    } else {
     plane.isFlying = false;
     this.planeHangar.push(plane);
     return this.planeHangar;
   }
-
+}
+  fullAirport(){
+    if(capacity === this.planeHangar.length){
+      return true 
+      } else {
+        return false;
+      }
+    }
 }
 
 module.exports = Airport;
