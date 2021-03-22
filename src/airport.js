bb8b2adafe71a09b5fd0d3228d7482d9714633c2
Sophie Lyon <sophielyon@MacBook-Pro-8.local>
2021-03-22 09:07:24 +0000

    uncompleted

--- src/airport.js
+++ src/airport.js
@@ -1,84 +1,43 @@
   // here's a starting point for you
   class Airport {
-  constructor(airportCapacity = 2){
+  constructor(airportCapacity = 3){
     this.planes  = []
     this.airportCapacity = airportCapacity
   }
 
   land(plane) {
+    if(this.isFull()) {
+      return 'Airport full do not land'
+    } else {
     this.planes.push(plane)
     return this.planes  
-  } 
-
-   /*checkPlane(plane) {
-    this.plane = plane
-    if(this.check(this.plane)) {
-      if(this.planes()) {
-        for(let i = 0; i < this.planes.length; i++) {
-          if(this.plane[i] === this.plane) {
-            return true
-          } else {
-            return false
-          }
-        }
-      }
     }
-  } */ 
+  } 
 
   isFull() {
     if(this.planes.length >= this.airportCapacity) {
       console.log('Airport full do not land')
       return true
     } else {
-      console.log('Airport capacity not reached, please land')
+      // console.log('Airport capacity not reached, please land')
       return false
   }
 }
 
 remove(plane) {
-    this.plane = plane
-    if(this.planes.includes(this.plane)) {
-      let index = this.planes.indexOf(this.plane)
+      let index = this.planes.indexOf(plane)
       this.planes.splice(index, 1)
       return this.planes 
+  }
+
+  check(allPlanes) {
+    if(allPlanes === this.planes) {
+      return true
     } else {
-      console.error('Airplane not at Airport')
       return false
-  }
-}
-}
-/*
-check(allPlanes) {
-  if(allPlanes === this.planes) {
-    return true
-  } else {
-    return false
-  }
-}
-/*
-checkPlaneRemoved(plane) {
-  this.plane = plane
-  if(this.check(this.plane)) {
-    if(this.planes()) {
-      for(let i = 0; i < this.planes.length; i++) {
-        if(this.plane[i] === this.plane) {
-          return false
-        } else {
-          return true
-        }
-      }
     }
   }
-} */
-/*
-check(allPlanes) {
-  if(allPlanes === this.planes) {
-    return true
-  } else {
-    return false
-  }
 }
-*/
 
 
 module.exports = Airport; 
--- src/plane.js
+++
@@ -1,7 +0,0 @@
-class Plane {
-    constructor(name){
-        this.name = name
-    }
-}
-
-module.exports = Plane;
