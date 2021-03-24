ffaeda274962bfee5d3ab6da2a6b6d5bf6d060fe
Lantana Ali <lantanaali20@gmail.com>
2021-03-24 12:30:38 +0000

    Converted existing tests into jasmine.

--- spec/airport.spec.js
+++ spec/airport.spec.js
@@ -1,47 +1,50 @@
-const testFramework = require('../test-framework');
 const Airport = require('../src/airport');
 const Plane = require('../src/plane.js');
 const assertEquals = require('../test-framework');
 
-console.log('Test to see if the airport can land a plane.')
-let airport = new Airport();
-let plane = new Plane('Plane One');
-let result = airport.landPlane(plane);
-console.log(assertEquals(result.length, 1));
-
-
-console.log('Testing if airport capacity can be overriden');
-airport = new Airport(20);
-result = airport.capacity;
-console.log(assertEquals(result, 20));
-
-console.log('Testing to see if we cannot land planes if airport is full');
-airport = new Airport(2);
-let capPlane = new Plane('cap');
-let capPlane2 = new Plane('capTwo');
-let capPlane3 = new Plane('capThree');
-
-airport.landPlane(capPlane);
-airport.landPlane(capPlane2);
-result = airport.landPlane(capPlane3);
-console.log(assertEquals(result, 'cannot land plane - aiport is full'));
-
-console.log('Testing to see if the plane has taken off');
-airport = new Airport();
-planeTwo = new Plane('Plane Two');
-airport.landPlane(planeTwo);
-result = airport.takeOff(planeTwo);
-console.log(assertEquals(result.length, 0));
-
-console.log('Testing to see if blocked from taking off airport that does not exist');
-airport = new Airport();
-planeThree = new Plane('Plane Three');
-result = airport.takeOff(planeThree);
-console.log(assertEquals(result, 'cannot takeoff plane that is not in the hangar'));
+describe('Testing airport functionality', function(){
+    it('Testing to see if the airport can land a plane', function(){
+        let airport = new Airport();
+        let plane = new Plane('Plane One');
+        let result = airport.landPlane(plane);
+        expect(result.length).toBe(1);
+    });
+    it('Testing if airport capacity can be overriden', function(){
+        let airport = new Airport(20);
+        let result = airport.capacity;
+        expect(result).toBe(20);
+    });
+    it('Testing to see if we cannot land planes if airport is full', function(){
+        let airport = new Airport(2);
+        let plane = new Plane('1');
+        let planeTwo = new Plane('2');
+        let planeThree = new Plane('3');
+        airport.landPlane(plane);
+        airport.landPlane(planeTwo);
+        result = airport.landPlane(planeThree);
+        expect(result).toBe('cannot land plane - airport is full');
+    });
+    it('Testing to see if aiport has successfully taken off a plane', function() {
+        let airport = new Airport();
+        let plane = new Plane('747');
+        airport.landPlane(plane);
+        result = airport.takeOff(plane);
+        expect(result.length).toBe(0);
+    });
+    it('Testing to see if blocked from taking off plane that doesnt exist', function() {
+        let airport = new Airport();
+        let plane = new Plane('Plane');
+        result = airport.takeOff(plane);
+        expect(result).toBe('cannot takeoff plane that is not in the hangar');
+    });
+    it('Testing to see if blocked from landing plane already in the hangar', function(){
+        let airport = new Airport();
+        let plane = new Plane('Plane Four');
+        airport.landPlane(plane);
+        result = airport.landPlane(plane);
+        expect(result).toBe('cannot land plane as it is already in the hangar');
+    });
+});
  
-console.log('Testing to see if blocked from landing a plane already in the hangar');
-aiport = new Airport();
-planeFour = new Plane('Plane Four');
-result = airport.landPlane(planeFour);
-result = airport.landPlane(planeFour);
-console.log(assertEquals(result, 'cannot land plane as it is already in the hangar'));
+
+
---
+++ spec/stormy.spec.js
@@ -0,0 +1,3 @@
+const Weather = require('../src/weather');
+// console.log('Preventing landing when it is stormy');
+// airport = new Airport();
