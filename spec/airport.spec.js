31cdd5fe5e70f144de7f18e81c5c25b33227858f
James Birchall <james.birchall4@btinternet.com>
2021-07-01 21:48:12 +0100

    Updated so that Plane is an object

--- spec/airport.spec.js
+++ spec/airport.spec.js
@@ -4,9 +4,11 @@ let assertEquals = require('../test-framework.js')
 //First test
 //1. Set up
 let airport = new Airport();
+let plane = {}
 
 //2. Execute
-let result = airport.landPlane();
+let result = airport.landPlane(plane);
+console.log(result);
 
 //3. Verify
-console.log(assertEquals(airport.planes.length, 1));
+console.log(assertEquals(result.length, 1));
