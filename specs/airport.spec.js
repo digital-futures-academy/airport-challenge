6022f6cefd9f20b6c6557de73f5c3c0d9426b53f
Ben Clayton <benclayton1@hotmail.com>
2021-07-02 22:06:12 +0100

    first failing test reached, Airport class not yet defined

---
+++ specs/airport.spec.js
@@ -0,0 +1,17 @@
+const assertEquals = require('./../test-framework.js');
+
+// Test to see whether a plane can land at an airport
+
+// Setup
+
+let airport = new Airport();
+let plane1 = 'Boeing 747';
+
+// Execute
+
+actualResult = airport.land(plane1);
+
+
+// Verify 
+
+console.log(assertEquals(plane1, actualResult));
