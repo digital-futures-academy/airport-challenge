defb314c1793e3612a108827647b309e2f8a948b
Colin <cln.gardiner@outlook.com>
2021-07-04 16:02:05 +0100

    added screenshots

--- src/airport.js
+++ src/airport.js
@@ -1,44 +1,44 @@
 //const Plane = require("./plane");
 
 class Airport {
-  // here's a starting point for you
-  constructor(name){
-    this.name = name;
-    this.planes = [];
-    // Default capacity is 20. 
-    this.capacity = 20; 
-  }
-
-  land (plane) {   
-   if (this.planes.length < this.capacity) {
-      plane.flyingStatus = 'landed';
-      this.planes.push(plane);
-      return "The plane has landed."
-    } else {
-      return "Sorry, unable to land, the aiport is full."
+    // here's a starting point for you
+    constructor(name){
+        this.name = name;
+        this.planes = [];
+        // Default capacity is 20. 
+        this.capacity = 20; 
     }
-  }
 
-  changeCapacity(newCapacity){
-    this.capacity = newCapacity; 
-  }
+    land (plane) {   
+        if (this.planes.length < this.capacity) {
+            plane.flyingStatus = 'landed';
+            this.planes.push(plane);
+            return 'The plane has landed.';
+        } else {
+            return 'Sorry, unable to land, the aiport is full.';
+        }
+    }
 
-  takeOff(plane){  
-    const planeBay = this.planes.indexOf(plane); 
-    const takeOffPlane = this.planes[planeBay];
+    changeCapacity(newCapacity){
+        this.capacity = newCapacity; 
+    }
 
-    console.log(planeBay)
-    console.log(takeOffPlane)
+    takeOff(plane){  
+        const planeBay = this.planes.indexOf(plane); 
+        const takeOffPlane = this.planes[planeBay];
+
+        console.log(planeBay);
+        console.log(takeOffPlane);
     
 
-      if (takeOffPlane.flyingStatus === 'take off'){
-        this.planes.splice(planeBay, 1);
+        if (takeOffPlane.flyingStatus === 'take off'){
+            this.planes.splice(planeBay, 1);
 
-        return `Plane ${takeOffPlane.name} has taken off. There are now ${this.planes.length} planes.`
-      }
+            return `Plane ${takeOffPlane.name} has taken off. There are now ${this.planes.length} planes.`;
+        }
       
     }
     
-  }
+}
 
 module.exports = Airport;
--- src/atc.js
+++ src/atc.js
@@ -3,26 +3,26 @@ class AirTrafficController{
 
     queryTakeOff(plane,airport){    
         if (airport.planes.includes(plane)) {
-            airport.planes[airport.planes.indexOf(plane)].flyingStatus = 'take off'
-            console.log(airport.planes)
-            return "Ready for take off!"
+            airport.planes[airport.planes.indexOf(plane)].flyingStatus = 'take off';
+            console.log(airport.planes);
+            return 'Ready for take off!';
         }
         else if (plane.flyingStatus === 'flying') {
-            return `The plane, ${plane.name}, you are trying to take off is already flying`
+            return `The plane, ${plane.name}, you are trying to take off is already flying`;
         }
         else {
-            return `The plane ${plane.name} is not located at ${airport.name}, take off request invalid.`
+            return `The plane ${plane.name} is not located at ${airport.name}, take off request invalid.`;
         }
     }
 
     queryLanded(plane, airport) {
         if (airport.planes.includes(plane)) {
-            return `The plane, ${plane.name}, you are trying to land has has already landed`
+            return `The plane, ${plane.name}, you are trying to land has has already landed`;
         } else {
-            return 'Ready for landing!'
+            return 'Ready for landing!';
         }
 
     }
 }   
 
-module.exports = AirTrafficController
+module.exports = AirTrafficController;
