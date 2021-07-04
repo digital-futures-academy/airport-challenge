154b37f3834f6a574ab4a4b2b3100e2e4833f16f
Nkosilathi Sibanda <kosi53@hotmail.co.uk>
2021-07-04 19:13:24 +0100

    Solution for test 2: Default airport capacity that can be overridden as appropriate.

--- TestFramework/spec/airport.spec.js
+++ TestFramework/spec/airport.spec.js
@@ -2,16 +2,38 @@ let assertEquals = require('../../test-framework');
 let Airport = require('../src/airport');
 let Plane = require('../src/plane');
 
+let plane, airport;
+let expectedResult, actualResult, result;
+
 console.log('Test 1: Instruct the airport to land a plane');
 
 //Set up
-let expectedResult = 'Boeing 777-300 plane has just landed.';
+expectedResult = 'Boeing 777-300 plane has just landed.';
 
 //Execute
-let plane = new Plane('Boeing 777-300', 'Malaysia', false, true, false);
-let airport = new Airport('British Airways');
-let actualResult = airport.land(plane); 
+plane = new Plane('Boeing 777-300', 'Malaysia', false, true, false);
+airport = new Airport('British Airways');
+actualResult = airport.land(plane); 
 
 //Verify
-let result = assertEquals(actualResult, expectedResult);
+result = assertEquals(actualResult, expectedResult);
 console.log(`Result: ${result}`);
+
+//-----------------------------------------------------------------------
+
+console.log('\nTest 2: Default airport capacity that can be overridden as appropriate');
+
+//Set up
+expectedResult = 'British Airways airport capacity is now 296';
+
+//Execute
+plane = new Plane('Boeing 777-300', 'Malaysia', false, true, true);
+airport = new Airport('British Airways');
+actualResult = airport.airportCapacity(296);
+
+//Verify
+result = assertEquals(actualResult, expectedResult);
+console.log(`Result: ${result}`);
+
+
+
--- TestFramework/src/airport.js
+++ TestFramework/src/airport.js
@@ -1,11 +1,17 @@
 class Airport {
     constructor(name) {
         this._name = name;
+        this._airportCapacity = 249; //airport operates a fleet of 249 aircraft
     }
 
     land(plane) {
         return `${plane._name} plane has just landed.`;
     }   
+
+    airportCapacity(capacity) {
+        this._airportCapacity = capacity;
+        return `${this._name} airport capacity is now ${this._airportCapacity}`;
+    }
 }
 
 module.exports = Airport;
