389c89d7f4719e779ccda937050d44df0d9e6756
RyanVanDijck <rvandijck02@outlook.com>
2021-07-04 22:45:17 +0100

    Setting up tests

---
+++ spec/test.spec.js
@@ -0,0 +1,12 @@
+class Test{
+    assertEquals(val1, val2){
+        return val1 === val2; 
+    }
+    it(testName, testFunction){
+        console.log(testName); 
+        console.log(testFunction); 
+        console.log(); 
+    }
+}
+
+module.exports = Test; 
