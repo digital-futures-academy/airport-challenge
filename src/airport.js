df46959ca68e17faabd3da5a83fadc0c302a4c7f
RyanVanDijck <rvandijck02@outlook.com>
2021-07-04 23:11:25 +0100

    Adding setCapacity method

--- src/airport.js
+++ src/airport.js
@@ -3,7 +3,17 @@ class Airport{
         this._capacity = capacity; 
         this.planes = [];  
     }
-    
+    setCapacity(capacity){
+        if (capacity < this.planes.length){
+            return 'More planes than capacity';
+        }
+        else if(capacity < 0){
+            return 'Cannot have less than one plane';
+        }
+        else{
+            this._capacity = capacity; 
+        }
+    }
 
 }
 module.exports =  Airport; 
