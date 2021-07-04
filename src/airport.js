ec3f024b13a754ae3c7294ec64762c68f089d522
RyanVanDijck <rvandijck02@outlook.com>
2021-07-04 23:17:09 +0100

    Adding land method

--- src/airport.js
+++ src/airport.js
@@ -15,5 +15,18 @@ class Airport{
         }
     }
 
+    land(plane){
+        if (this.planes.length >= this._capacity){
+            return 'Airport Full';
+        }
+        else if(this.planes.includes(plane)){
+            return 'Plane present in airport'; 
+        }
+        else{
+            this.planes.push(plane); 
+            return this.planes; 
+        }
+    }
+
 }
 module.exports =  Airport; 
