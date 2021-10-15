bc42443fa1492f56edeb4073323da93e60a0126c
Kieran Munslow <kieranmunslow@icloud.com>
2021-10-15 22:05:49 +0100

    Wrote first failing test which involves testing to make sure that a request can't be made for a plane to take off which isn't landed at the airport

--- spec/specRunner.js
+++ spec/specRunner.js
@@ -2,3 +2,4 @@ require('./landPlane.spec');
 require('./defaultCapacity.spec');
 require('./capacityPreventLanding.spec');
 require('./planeTakeOff.spec');
+require('./verifyRequests.spec')
---
+++ spec/verifyRequests.spec.js
@@ -0,0 +1,21 @@
+const assertEquals = require('./test-framework');
+const Airport = require('../src/airport');
+const Plane = require('../src/Plane');
+
+console.log('--- VERIFY TAKE-OFFS AND LANDINGS ---');
+
+let expectedOutput, result;
+
+// Test 1 - Test to check that a request for a plane that isn't at the airport to take off can't be made
+console.log('Test 1 - Test to check that a request for a plane that isn\'t at the airport to take off can\'t be made');
+// Arrange
+let airport = new Airport();
+let plane1 = new Plane();
+let plane2 = new Plane();
+expectedOutput = 1;
+// Act
+airport.landPlane(plane1);
+airport.allowTakeOff(plane2);
+// Assert
+result = assertEquals(airport.planes.length, expectedOutput);
+console.log(result)
