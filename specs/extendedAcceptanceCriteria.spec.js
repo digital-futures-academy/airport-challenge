6830247208b7496c86cf29590014ee545b26fa78
tangjm <jaredtang@hotmail.co.uk>
2021-10-15 15:41:28 +0100

    Created failing test7

--- specs/extendedAcceptanceCriteria.spec.js
+++ specs/extendedAcceptanceCriteria.spec.js
@@ -17,3 +17,19 @@ test.it("Test6 - Testing that we can prevent takeoff when weather is 'stormy'", 
 	// Assert
 	test.assertEquals(actualOutput.toString(), expectedOutput.toString());
 })
+
+test.it("Test7 - Testing that we can prevent landing when weather is 'stormy'", () => {
+	// Assign
+	let actualOutput, expectedOutput;
+	let plane = new Plane();
+	let airport = new Airport();
+	airport.parkingLot = [];
+	airport.weather = "stormy";
+	expectedOutput = [];
+
+	// Act
+	actualOutput = airport.landPlane(plane);
+
+	// Assert
+	test.assertEquals(actualOutput.toString(), expectedOutput.toString());
+})
