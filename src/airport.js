4891372b36a1a0d40a235d4f3ee720be98ffd766
KatieStocker <stocker_katie@aol.co.uk>
2021-10-15 14:56:49 +0100

    Added source code to pass the test of Criteria 3 - passes due to the length not being increased past the default capacity set

--- src/airport.js
+++ src/airport.js
@@ -1,15 +1,33 @@
 class Airport {
 
   planes;
-  defaultCapacity;
+  defaultCapacity = 5;
+  status = false;
 
-  constructor(planes = [], defaultCapacity = 0) {
+  constructor(planes = [], defaultCapacity = 10) {
     this.planes = planes;
     this.defaultCapacity = defaultCapacity;
+
+    this.#updateAirportStatus();
+  }
+
+  #updateAirportStatus() {
+    if (this.planes.length >= this.defaultCapacity) {
+      this.status = true;
+    }
+    else {
+      this.status = false;
+    }
   }
 
   landPlane(plane) {
-    this.planes.push(plane);
+    if (!this.status && (this.planes.length + 1) <= this.defaultCapacity) {
+      this.planes.push(plane);
+      this.#updateAirportStatus();
+    }
+    else {
+      console.log("Unable to land plane, airport is full.");
+    }
   }
 
   updateDefaultAirportCapacity(value = int) {
