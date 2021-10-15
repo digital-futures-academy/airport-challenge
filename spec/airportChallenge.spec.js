b5e763ef5470a6e01da629514524015fb0d6f7dc
Hamsa Mohammed <hamsam1197@gmail.com>
2021-10-15 10:59:28 +0100

    write and pass 3rd test

--- spec/airportChallenge.spec.js
+++ spec/airportChallenge.spec.js
@@ -30,3 +30,18 @@ test.it('Check if airport planes array increases by one after landing a plane', 
   result = heathrow.currentPlanes.length;
   test.assertEquals(expectedOutput, result);
 })
+
+test.it('Check if airport adds specific plane to its array after landing plane', () => {
+
+  let expectedOutput;
+  let result;
+
+  let heathrow = new Airport();
+  let boeing1 = new Airplane('boeing747-1');
+  heathrow.landPlane(boeing1);
+
+  expectedOutput = boeing1;
+  result = heathrow.currentPlanes[heathrow.currentPlanes.length - 1];
+  console.log(result);
+  test.assertEquals(expectedOutput, result);
+})
