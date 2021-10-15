a6349287719b2819f3f275a0d6f942436077e0d8
niconealon <nico.nealon1999@gmail.com>
2021-10-15 11:22:00 +0100

    ...

---
+++ spec/airport.spec.js
@@ -0,0 +1,12 @@
+const assertEquals = require("./test-framework");
+
+
+
+// Test 1- Is plane
+let airport, plane, expectedOutput, result;
+airport = new airport();
+plane = new Plane();
+expectedOutput = 1
+airport.addPlane(plane);
+result = assertEquals(airport.planes.length, expectedOutput)
+console.log(result)
