e9db0524e7119a21f3283539d1d1d3c93a1d908b
tangjm <jaredtang@hotmail.co.uk>
2021-10-15 11:56:22 +0100

    Refactored test1

--- specs/acceptanceCriteria.spec.js
+++ specs/acceptanceCriteria.spec.js
@@ -1,20 +1,20 @@
 const Airport = require('../src/Airport');
-const assertEquals = require('../test-framework');
 const Plane = require('../src/Plane');
+const test = require('../test-framework');
 
 // Test1
-console.log("Test1 - Testing that we can land planes");
-// Step1 - Arrange
-let actualOutput, expectedOutput, result;
+test.it("Test1 - Testing that we can land planes", () => {
+	// Step1 - Arrange
+	let actualOutput, expectedOutput;
+	let airport = new Airport();
+	let plane = new Plane();
+	expectedOutput = [plane];
 
-let airport = new Airport();
-let plane = new Plane();
-expectedOutput = [plane];
+	// Step2 - Act
+	actualOutput = airport.landPlane(plane);
 
-// Step2 - Act
-actualOutput = airport.landPlane(plane);
+	// Step3 - Assert
+	// seems like js cannot compare arrays for identity
+	test.assertEquals(actualOutput.toString(), expectedOutput.toString());
+})
 
-// Step3 - Assert
-// seems like js cannot compare arrays for identity
-result = assertEquals(actualOutput.toString(), expectedOutput.toString());
-console.log(result);
--- test-framework.js
+++ test-framework.js
@@ -1,3 +1,22 @@
-const assertEquals = (actual, expected) => actual === expected;
+const test = {
+	assertEquals(actual, expected) {
+		if (actual !== expected) throw new Error("Test failed");
+	},
 
-module.exports = assertEquals;
+	it(testMessage, testFunc) {
+		try {
+			testFunc();
+			console.log(testMessage);
+		} catch (err) {
+			console.log(testMessage);
+			console.log(err.message);
+			console.log(err.stack);
+		}
+	},
+
+	xit(message) {
+		console.log("Test skipped");
+	}
+}
+
+module.exports = test;
