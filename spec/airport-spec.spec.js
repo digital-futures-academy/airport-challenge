1852baf56a2afb8a0a50ef3a640d7ef8be4dcb8b
KatieStocker <stocker_katie@aol.co.uk>
2021-10-15 14:01:42 +0100

    Refactored my code in the test-framework file to use a test function, altered code in the airport-spec file to fit with this change.

--- spec/airport-spec.spec.js
+++ spec/airport-spec.spec.js
@@ -1,18 +1,18 @@
 const Airport = require('../src/airport.js');
-const assertEquals = require('../test-framework.js');
+const test = require('../test-framework.js');
 
-let airport;
-let planes = new Array();
+let airport, planes = new Array();
 
-console.log('Criteria 1: land plane');
-//Setup (Arrange)
-airport = new Airport(planes);
-let expectedOutput = 1;
 
-//Execute (Act)
-airport.landPlane('EasyJet');
-let actualOutput = airport.planes.length;
+test.it("Criteria 1 - land a plane.", () => {
+    //Setup (Arrange)
+    airport = new Airport(planes);
+    let expectedOutput = 1;
 
-//Verify (Assert)
-let result = assertEquals(actualOutput, expectedOutput);
-console.log(result);
+    //Execute (Act)
+    airport.landPlane('EasyJet');
+    let actualOutput = airport.planes.length;
+
+    //Verify (Assert)
+    test.assertEquals(actualOutput, expectedOutput);
+})
--- test-framework.js
+++ test-framework.js
@@ -1,3 +1,18 @@
-const assertEquals = (val1, val2) => val1 === val2;
+const test = {
+    assertEquals(val1, val2) {
+        if (val1 !== val2) throw new Error(`ActualOutput: ${val1} and ExpectedOutput: ${val2}, were not equal.`);
+    },
 
-module.exports = assertEquals;
+    it(text, testFunc) {
+        try {
+            testFunc();
+            console.log("\x1b[32m%s\x1b[0m", `\tPassed: ${text}`);
+        }
+        catch (error) {
+            console.log("\x1b[31m%s\x1b[0m", `\tFailed: ${text}`);
+            console.error(error.message);
+        }
+    }
+}
+
+module.exports = test;
