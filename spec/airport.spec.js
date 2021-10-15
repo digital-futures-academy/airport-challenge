f257b80707326123424c15abc0c4758673f4ea6b
Nikesh-A97 <Nikeshasokcumar@hotmail.com>
2021-10-15 15:18:38 +0100

    update spec and airport
    added more tests
    added more methods to Airport

--- spec/airport.spec.js
+++ spec/airport.spec.js
@@ -2,7 +2,6 @@ const Airport = require('../src/airport');
 const Plane = require('../src/plane');
 const { assertEquals, printResult } = require('../test-framework');
 
-
 let airport, expectedOutput, actualOutput, plane;
 
 //Arrange
@@ -17,4 +16,78 @@ airport.landPlane(plane);
 actualOutput = assertEquals(expectedOutput, airport.planes.length);
 printResult('testing if 1 plane in airport', actualOutput);
 
+expectedOutput = null;
+//-------------------------------------------------------------------------//
+
+//Arrange
+expectedOutput = 0;
+
+//Act
+airport.sendPlane(plane);
+
+//Assert
+actualOutput = assertEquals(expectedOutput, airport.planes.length);
+printResult('remove plane from airport', actualOutput);
+
+expectedOutput = null;
+//-------------------------------------------------------------------------//
+
+let plane1, plane2;
+
+//Arrange
+plane1 = new Plane();
+plane2 = new Plane();
+expectedOutput = 2;
+
+//Act
+airport.landPlane(plane1);
+airport.landPlane(plane2);
+
+//Assert
+actualOutput = assertEquals(expectedOutput, airport.planes.length);
+printResult('testing if 2 planes in airport', actualOutput);
+
+expectedOutput = null;
+//-------------------------------------------------------------------------//
+
+//Arrange
+expectedOutput = 1;
+
+//Act
+airport.sendPlane(plane2);
+
+//Assert
+actualOutput = assertEquals(expectedOutput, airport.planes.length);
+printResult('Testing if removing 1 plane with 2 planes in airport', actualOutput);
+
+expectedOutput = null;
+
+//-------------------------------------------------------------------------//
+
+//Arrange
+expectedOutput = 2;
+
+//Act
+airport.landPlane(plane2);
+airport.sendPlane(plane);
+
+//Assert
+actualOutput = assertEquals(expectedOutput, airport.planes.length);
+printResult('Testing if removing plane not in airport with 2 planes', actualOutput);
+
+expectedOutput = null;
+
+//-------------------------------------------------------------------------//
+
+//Arrange
+expectedOutput = 2;
+
+//Act
+airport.landPlane(plane2);
+
+//Assert
+actualOutput = assertEquals(expectedOutput, airport.planes.length);
+printResult('Testing if landing a plane that already exists in airport', actualOutput);
+
+expectedOutput = null;
 
--- src/airport.js
+++ src/airport.js
@@ -4,7 +4,29 @@ class Airport {
     }
 
     landPlane(plane) {
-        this.planes.push(plane);
+        const indx = this.findPlane(plane);
+        if (indx === -1) {
+            this.planes.push(plane);
+        } else {
+            console.log('Plane is already at airport');
+        }
+    }
+
+    sendPlane(plane) {
+        const indx = this.findPlane(plane);
+        if (indx === -1) {
+            console.log('Plane does not exist in airport');
+        } else {
+            this.planes.splice(indx, 1);
+        }
+    }
+
+    findPlane(plane) {
+        return this.planes.indexOf(plane);
+    }
+
+    isPlaneInAirport(plane) {
+        return this.planes.indexOf(plane) === -1 ? false : true;
     }
 }
 
