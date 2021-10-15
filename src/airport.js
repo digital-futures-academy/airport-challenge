da021501cc32c6b3094c7345334d49c6438f3e78
tangjm <jaredtang@hotmail.co.uk>
2021-10-15 12:20:53 +0100

    Test2 now passes; Added a 'capacity' property to Airport that can be overriden with a constructor

--- src/airport.js
+++ src/airport.js
@@ -1,6 +1,11 @@
 class Airport {
   // here's a starting point for you
   parkingLot = [];
+  capacity;
+
+  constructor(newCapacity = 5) {
+    this.capacity = newCapacity;
+  }
 
   landPlane(plane) {
     this.parkingLot.push(plane);
