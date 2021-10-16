14a956c633eabe1f9abd8d1d34f05d77705cac42
tangjm <jaredtang@hotmail.co.uk>
2021-10-16 11:44:01 +0100

    finished refactoring the tests for the main acceptance criteria

--- specs/acceptanceCriteria.spec.js
+++ specs/acceptanceCriteria.spec.js
@@ -80,7 +80,6 @@ test.it("Test4 - Testing that we can get planes to take off", () => {
 	actualOutput = airport.takeOff(plane);
 
 	// Step3 - Assert
-	// seems like js cannot compare arrays for identity
 	test.assertEquals(actualOutput, expectedOutput);
 })
 
