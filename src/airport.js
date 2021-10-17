a9ccd88adc92564a3d46720a868479b339e42b19
Nikesh-A97 <Nikeshasokcumar@hotmail.com>
2021-10-17 23:15:23 +0100

    first test
    first test passed, wrote the landPlane() and getCurrentCapacity() methods in order to make the first failing test pass.

--- src/airport.js
+++ src/airport.js
@@ -1,5 +1,15 @@
 class Airport {
+    constructor(listPlanes = []) {
+        this.listPlanes = listPlanes;
+    }
 
+    landPlane(plane) {
+        this.listPlanes.push(plane);
+    }
+
+    getCurrentCapacity() {
+        return this.listPlanes.length;
+    }
 }
 
 module.exports = Airport;
