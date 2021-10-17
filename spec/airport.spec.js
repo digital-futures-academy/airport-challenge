3827d4fb5d40209391d9f8f2f3b8b9a04fd67b16
Nikesh-A97 <Nikeshasokcumar@hotmail.com>
2021-10-17 23:09:46 +0100

    first test failed
    to be  expected

--- spec/airport.spec.js
+++ spec/airport.spec.js
@@ -2,6 +2,14 @@ const Airport = require('../src/airport');
 const Plane = require('../src/plane');
 const { assertEquals, printResult } = require('../test-framework');
 
-let airport, expectedOutput, actualOutput, plane;
+let airport, expectedOutput, actualOutput, plane, testMsg;
 
+//Act
+plane = new Plane();
+airport = new Airport();
+testMsg = "Testing to land a plane so there must be 1 plane at the airport";
+expectedOutput = 1;
 
+//Assert
+actualOutput = assertEquals(expectedOutput, airport.getCurrentCapacity());
+printResult(testMsg, actualOutput);
