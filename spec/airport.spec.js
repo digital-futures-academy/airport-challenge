45161f6b6c7d929e49b5dc69208f8ab6b9b15c7d
Nikesh-A97 <Nikeshasokcumar@hotmail.com>
2021-10-17 23:12:59 +0100

    Update airport.spec.js

--- spec/airport.spec.js
+++ spec/airport.spec.js
@@ -4,11 +4,14 @@ const { assertEquals, printResult } = require('../test-framework');
 
 let airport, expectedOutput, actualOutput, plane, testMsg;
 
-//Act
+//Arrange
 plane = new Plane();
 airport = new Airport();
 testMsg = "Testing to land a plane so there must be 1 plane at the airport";
 expectedOutput = 1;
+
+//Act
+airport.landPlane(plane);
 
 //Assert
 actualOutput = assertEquals(expectedOutput, airport.getCurrentCapacity());
