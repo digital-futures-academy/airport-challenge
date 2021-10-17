44a9bd5a47c366dfbc84067a2068d6a00fb3641e
MatthewHWN <91743978+MatthewHWN@users.noreply.github.com>
2021-10-17 18:25:17 +0100

    added weather control

---
+++ spec/weather.spec.js
@@ -0,0 +1,18 @@
+
+const Weather = require('../src/weather');
+const test2 = require('../test-framework');
+
+test2.it("Weather is clear - plane checks can continue", () => {
+
+    //setup
+    weather = new Weather();
+    let actualOutput;
+    expectedOutput = false
+
+    //execute
+    actualOutput = Boolean(weather.isClear())
+
+    //verify
+    test2.assertEquals(actualOutput, expectedOutput);
+
+});
---
+++ src/weather.js
@@ -0,0 +1,19 @@
+// Separate from airport.js as a random variable, not controlled by the airport.
+
+class Weather {
+
+    isClear = (integer) => {
+
+        let storm = 0;
+
+        let weather = (Math.floor((Math.random() * 10) + 1))
+        if (weather > 7) {
+            storm = 1;
+        }
+        return storm;
+
+    };
+
+};
+
+module.exports = Weather;
