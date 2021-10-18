7dde29c33cc21737c0b2b459e197cce8e85d8b66
Nikesh-A97 <Nikeshasokcumar@hotmail.com>
2021-10-18 10:27:20 +0100

    Update test-framework.js

--- test-framework.js
+++ test-framework.js
@@ -1,11 +1,13 @@
 const assertEquals = (val1, val2) => { return (val1 === val2); };
 
 const printResult = (message = 'No Message', result) => {
-    console.log('=============================================================================');
     let color = null;
+    const resetCol = '\x1b[0m';
     result ? color = '\x1b[32m' : color = '\x1b[31m';
+
+    console.log('=============================================================================');
     console.log('Testing: ' + message + '\nThe result is : ' + color + result);
-    console.log('\x1b[0m', '=============================================================================\n\n');
+    console.log(resetCol + '=============================================================================\n\n');
 };
 
 //module.exports = assertEquals;
