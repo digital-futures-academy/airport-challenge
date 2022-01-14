7d24aea75ea622d5ff947ee7a1f7d6ec4fdffd58
RSmith-TD <rogan.smith@hotmail.co.uk>
2022-01-14 19:39:18 +0000

    updated plane for new spec

--- src/plane.js
+++ src/plane.js
@@ -1,12 +1,12 @@
 class Plane {
     // here's a starting point for you
-    constructor() {
-
+    constructor(planeID) {
+        this.planeID = planeID
     }
 
-    land(landingAirport) {
-        landingAirport.landedPlanes.push(this);
-    }
+    // land(landingAirport) {
+    //     landingAirport.landedPlanes.push(this);
+    // }
 }
 
 module.exports = Plane;
