feb18442cccd25484c0bf2a3628eca6bff95ec44
RSmith-TD <rogan.smith@hotmail.co.uk>
2022-01-14 17:09:33 +0000

    added some assert functions

--- spec/test-framework.js
+++ spec/test-framework.js
@@ -1,5 +1,17 @@
-const assertEquals = (actual, expected) => {
-    return actual === expected;
+const assertEquals = (actual, expected) => actual === expected;
+
+//Type checks
+const assertString = (varToCheck) => typeof varToCheck === "string";
+const assertNumber = (varToCheck) => typeof varToCheck === "number";
+const assertBool = (varToCheck) => typeof varToCheck === "boolean";
+const assertArray = (varToCheck) => Array.isArray(varToCheck);
+
+const assertStatements = {
+    assertEquals,
+    assertString,
+    assertNumber,
+    assertBool,
+    assertArray
 }
 
-module.exports = { assertEquals };
+module.exports = assertStatements;
