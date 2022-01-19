2a13a74206d92d4172e92d6fdab9710690fbbbba
James Sutton <jamesalexandersutton1@gmail.com>
2022-01-19 14:41:59 +0000

    Failing tests for user story 2

--- spec/Airport.spec.js
+++ spec/Airport.spec.js
@@ -8,3 +8,14 @@ test.it('test airport can land plane', function () {
     airport.landPlane(plane);
     return test.expect(airport.planes.length).toEqual(1);
 });
+
+test.it('test default value for airport capacity is set', function () {
+    let airport = new Airport();
+    return test.expect(airport.capacity).toEqual(10);
+});
+
+test.it('test airport capacity can be overridden', function () {
+    let airport = new Airport(15);
+    airport.updateCapacity(5);
+    return test.expect(airport.capacity).toEqual(20);
+});
