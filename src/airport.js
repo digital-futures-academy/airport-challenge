a1e8cc6c57220089509f61e8a206efcc736cf70f
samrowlands <sam.rowlands@bath.edu>
2022-01-19 08:58:00 +0000

    Passed Test Two

--- src/airport.js
+++ src/airport.js
@@ -1,5 +1,17 @@
 class Airport {
   // here's a starting point for you
+  constructor(capacity, airspace, airplanes) {
+    this.capacity = capacity;
+    this.airspace = airspace;
+    this.airplanes = airplanes;
+  }
+
+  land(tailNumber) {
+    if (!this.airspace.includes(tailNumber)) {
+      return `Landing unsuccessful; ${tailNumber} cannot be found in airspace`
+    }
+  }
+
 
 }
 
