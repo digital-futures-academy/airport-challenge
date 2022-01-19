8d3601a84fee2917522f5475bf083150840b534e
samrowlands <sam.rowlands@bath.edu>
2022-01-19 09:07:58 +0000

    Passed Test Three

--- test/airport.spec.js
+++ test/airport.spec.js
@@ -5,11 +5,14 @@ console.log(`Testing that trying to land plane 'AH574' when not in airspace outp
 
 //1. Arrange
 tailNumber = 'AH574';
+airspace = []
+capacity = 1
+airplanesAtAirport = []
 expectedOutput = 'Landing unsuccessful; AH574 cannot be found in airspace';
 
 //2. Act
-airport = new Airport(1, [], []);
-actualOutput = airport.land('AH574');
+airport = new Airport(capacity, airspace, airplanesAtAirport);
+actualOutput = airport.land(tailNumber);
 
 //3. Assert 
 result = assertEquals(actualOutput, expectedOutput);
@@ -19,14 +22,38 @@ console.log(result);
 console.log(`Testing that landing plane AH574 in a max capacity airport will output 'Landing unsuccessful; Airport is full'`)
 
 //1. Arrange
+tailNumber = 'AH574'
+airspace = ['AH574']
 airportCapacity = 0;
+airplanesAtAirport = []
 expectedOutput = 'Landing unsuccessful; Airport is full';
 
 //2. Act
-airport = new Airport(airportCapacity, ['AH574'], []);
-actualOutput = airport.land('AH574')
+airport = new Airport(airportCapacity, airspace, []);
+actualOutput = airport.land(tailNumber)
 
 //3. Assert 
 result = assertEquals(actualOutput, expectedOutput);
 console.log(result);
+
+console.log(`Testing that landing 'AH574' outputs 'Landing successful; AH574 has arrived at airport'`);
+
+//1. Arrange
+
+tailNumber = 'AH574';
+airspace = ['AH574']
+airportCapacity = 1;
+airplanesAtAirport = []
+expectedOutput = 'Landing successful; AH574 has arrived at airport';
+
+//2. Act
+airport = new Airport(airportCapacity, [tailNumber], []);
+actualOutput = airport.land(tailNumber);
+
+//3. Assert 
+result = assertEquals(actualOutput, expectedOutput);
+console.log(result);
+
+
+
 
