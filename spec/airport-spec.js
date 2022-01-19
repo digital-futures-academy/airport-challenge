5aa5df17bf2a6ba6b23dba06f499218011d38bb6
Naomi Mugglestone <NJLM2708@outlook.com>
2022-01-19 13:01:11 +0000

    commit on 'main'

--- spec/airport-spec.js
+++ spec/airport-spec.js
@@ -1,36 +1,6 @@
-const { airport } = require('.../src/airport')
+const Airport = require('./src/airport')
 
-
-/*let input, expectedOutput, actualOutput, result;
-// --------------------------------------------------
-console.log("countEventNumbersInArray function test");
-// --------------------------------------------------
-console.log("Testing that [1,2,3,4,5,6] => 3");
-// 1. Setup/Arrange
-input = [1, 2, 3, 4, 5, 6];
-expectedOutput = 3;
-
-// 2. Execute/Act
-actualOutput = countEvenNumbersInArray(input);
-
-// 3. Verify/Assert
-result = assertEquals(actualOutput, expectedOutput);
-console.log(result);
-
-As an air traffic controller
+/*As an air traffic controller
 So I can get passengers to a destination
 I want to instruct the airport to land a plane
 */
-
-const assertEquals = (landed, planeLanded) => {
-    return landed === planeLanded
-}
-
-const assertEquals = (stormy, weatherConditions) => {
-    return stormy === weatherConditions
-}
-
-const assertEquals = (Full, airportCapacity) => {
-    return
-}
-module.exports = assertEquals;
