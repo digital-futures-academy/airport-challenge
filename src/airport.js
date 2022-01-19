a846f2959d86acb6ef1ce448f832caf06b3800bc
samrowlands <sam.rowlands@bath.edu>
2022-01-19 09:19:31 +0000

    User Story 1

--- src/airport.js
+++ src/airport.js
@@ -8,11 +8,15 @@ class Airport {
 
   land(tailNumber) {
     if (!this.airspace.includes(tailNumber)) {
-      return `Landing unsuccessful; ${tailNumber} cannot be found in airspace`
+      return `Landing unsuccessful; ${tailNumber} cannot be found in airspace`;
+    } else if (!this.capacity > this.airplanes.length) {
+      return `Landing unsuccessful; Airport is full`;
+    } else if (this.airspace.includes(tailNumber) && this.capacity > this.airplanes.length) {
+      this.airplanes.push(tailNumber);
+      this.airspace = this.airspace.filter(e => e !== tailNumber)
+      return `Landing successful; ${tailNumber} has arrived at airport`;
     }
   }
-
-
 }
 
 module.exports = Airport;
