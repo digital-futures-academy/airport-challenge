f0413d708586dbbaf23e2d80ffa69122b591235e
gabiblack <gabiblack22@gmail.com>
2022-01-20 16:03:33 +0000

    Wrote first failing test

---
+++ spec/Airport.spec.js
@@ -0,0 +1,14 @@
+const Airport = require('../src/Airport');
+const Plane = require(`../src/Plane`);
+
+describe(`Airport Tests: `, () => {
+    it(`land a plane`, () => {
+        // Arrange
+        const airport = new Airport();
+        const plane = new Plane();
+        // Act
+        airport.land(plane);
+        // Assert
+        expect(airports.onGround).toEqual(1);
+    });
+})
