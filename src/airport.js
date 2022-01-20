7bc6bf0d5b33d8add4cbbd8f4766a1fda3c24915
Lawrence Witt <lawrence@itt.dev>
2022-01-20 11:38:02 +0000

    make airport fields private

--- src/airport.js
+++ src/airport.js
@@ -1,47 +1,62 @@
 const Weather = require('./weather');
 
 class Airport {
+    #capacity;
+    #planes = [];
+    #forecaster;
+
     constructor(capacity = 10, forecaster = new Weather()) {
-        this.capacity = capacity;
-        this.planes = [];
-        this.forecaster = forecaster;
+        this.#capacity = capacity;
+        this.#forecaster = forecaster;
     }
 
     arrive(plane) {
-        const forecast = this.forecaster.forecast();
+        const forecast = this.#forecaster.forecast();
 
         if (forecast === 'stormy')
             throw new Error('Cannot recieve a Plane while weather is stormy.');
-        if (this.capacity === this.planes.length)
+        if (this.isFull())
             throw new Error('Cannot recieve a Plane while capacity is full.');
         if (plane.status !== 'arriving')
             throw new Error('Cannot recieve a Plane which is not arriving.');
 
-        this.planes.push(plane);
+        this.#planes.push(plane);
     }
 
     depart(plane) {
-        const forecast = this.forecaster.forecast();
+        const forecast = this.#forecaster.forecast();
 
         if (forecast === 'stormy')
             throw new Error('Cannot release a Plane while weather is stormy.');
 
-        const idx = this.planes.findIndex((p) => p.id === plane.id);
+        const idx = this.#planes.findIndex((p) => p.id === plane.id);
 
         if (idx === -1)
             throw new Error('Cannot release a Plane which is not at this Airport.');
         if (plane.status !== 'departing')
             throw new Error('Cannot release a Plane which is not departing.');
 
-        this.planes.splice(idx, 1);
+        this.#planes.splice(idx, 1);
     }
 
     isFull() {
-        return this.capacity === this.planes.length;
+        return this.#capacity === this.#planes.length;
     }
 
     hasPlane(plane) {
-        return this.planes.some((p) => p.id === plane.id);
+        return this.#planes.some((p) => p.id === plane.id);
+    }
+
+    get capacity() {
+        return this.#capacity;
+    }
+
+    get planes() {
+        return this.#planes;
+    }
+
+    get forecaster() {
+        return this.#forecaster;
     }
 }
 
