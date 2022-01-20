6a80388bfaa0f0e5c340f1974a6dbb9eb47c5709
Matt <wyattmatt@protonmail.com>
2022-01-20 14:32:59 +0000

    Test runner refactored

--- spec/test-runner.js
+++ spec/test-runner.js
@@ -1,26 +1,33 @@
-import * as p from "./plane.spec.js";
-import * as a from "./airport.spec.js";
+import * as planeTests from "./plane.spec.js";
+import * as airportTests from "./airport.spec.js";
 
 // Plane class tests
-p.planeInitialisesWithNameTest();
-p.planeInitialisesWithAirportProperty();
-p.planeInitialisesWithDefaultAirportProperty();
+for (let property in planeTests) {
+    planeTests[property]();
+}
+// p.planeInitialisesWithNameTest();
+// p.planeInitialisesWithAirportProperty();
+// p.planeInitialisesWithDefaultAirportProperty();
 
 
 
 // Airport class tests
-a.airportInitialisesWithNameTest();
-a.airportInitialisesWithCapacityTest();
-a.airportInitialisesWithDefaultCapacityTest();
-a.airportCapacityCanBeChanged();
-a.airportInitialisesWithListOfPlanes();
-a.airportCanBeInitialisedWithCustomListOfPlanes();
-a.airportCannotLandPlaneWhenFull();
-a.airportCanLandPlaneWhenNotFull();
-a.airportCanLetPlaneTakeOff();
-a.airportCannotTakeOffPlaneNotInAirport();
-a.airportCannotLandPlaneAlreadyInAirport();
-a.airportLandPlaneReturnsPlaneObject();
-a.airportTakeOffPlaneReturnsPlaneObject();
-a.airportLandPlaneChangesPlaneAirportProperty();
-a.airportTakeOffPlaneChangesPlaneAirportProperty();
+for (let property in airportTests) {
+    airportTests[property]();
+}
+// a.airportInitialisesWithNameTest();
+// a.airportInitialisesWithCapacityTest();
+// a.airportInitialisesWithDefaultCapacityTest();
+// a.airportCapacityCanBeChanged();
+// a.airportInitialisesWithListOfPlanes();
+// a.airportCanBeInitialisedWithCustomListOfPlanes();
+// a.airportCannotLandPlaneWhenFull();
+// a.airportCanLandPlaneWhenNotFull();
+// a.airportCanLetPlaneTakeOff();
+// a.airportCannotTakeOffPlaneNotInAirport();
+// a.airportCannotLandPlaneAlreadyInAirport();
+// a.airportLandPlaneReturnsPlaneObject();
+// a.airportTakeOffPlaneReturnsPlaneObject();
+// a.airportLandPlaneChangesPlaneAirportProperty();
+// a.airportTakeOffPlaneChangesPlaneAirportProperty();
+
