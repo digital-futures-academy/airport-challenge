44640da4a668af129451b7b3a9fdeb4b402e1eff
Alistair Gibb <aligibb_uk@hotmail.com>
2022-06-10 14:58:32 +0100

    Add testFunctions.js to hold testing functions. Includes assertEquals function.

---
+++ testFunctions.js
@@ -0,0 +1,9 @@
+// Testing functions for export and use in tests e.g. function.spec.js
+
+// test comment on already staged file
+
+const assertEquals = (val1, val2) => val1 === val2;
+
+module.exports = {
+    assertEquals
+}
