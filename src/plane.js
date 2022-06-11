3a993668cbd0464d9c0a38d77e297b5707f70631
Alistair Gibb <aligibb_uk@hotmail.com>
2022-06-11 10:59:06 +0100

    Refactor landPlane.spec to initialise instance of Plane and Airport that can be reused throught tests.

---
+++ src/plane.js
@@ -0,0 +1,9 @@
+class Plane {
+    constructor() {
+
+    }
+}
+
+module.exports = {
+    Plane: Plane
+}
--- test/landPlane.spec.js
+++ test/landPlane.spec.js
@@ -3,10 +3,16 @@
 // Import any functions or frameworks needed e.g. assertEquals 
 const { assertEquals } = require('../testFunctions');
 const { Airport } = require('../src/Airport');
+const { Plane } = require('../src/Plane')
 
 // define variables that can be shared across all tests here to try and keep neat
 let input, expectedOutput, actualOutput, result;
 let testName = ``;
+
+// set up new airport and plane object that can be used within multiple tests.
+const airport = new Airport();
+const plane = new Plane();
+
 
 console.log(`=====landPlane.spec.js test results=====`)
 
@@ -16,7 +22,7 @@ console.log(`=====landPlane.spec.js test results=====`)
 testName = `Test 1: Method "landPlane" exists in class instance`;
 
 // Create an instance of the Airport class as input. Define the expectedOutput as being the expected variable, we expect true as intend to use object.hasOwnProperty() to check, which returns a boolean.
-input = new Airport();
+input = airport;
 expectedOutput = true;
 
 // Act - Check whether the instance of the Airport class contains 'landPlane' using hasOwnProperty() and assign to actual output
@@ -25,3 +31,9 @@ actualOutput = input.hasOwnProperty('landPlane');
 // Assert - use assertEquals to check whether expected and actual outputs match. Print the result to the console.
 result = assertEquals(actualOutput, expectedOutput);
 console.log(`${testName}: ${result ? `PASS` : `FAIL`}`)
+
+
+// // Test 2 - On providing a Plane as an argument landPlane returns 'Success'.
+// input = new Plane();
+// expectedOutput = 'success'
+// actualOutput = 
