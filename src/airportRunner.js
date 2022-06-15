0144c501667c1c60aa8164fb4a4517c830d38565
pranay-a-1 <pranay.a4795@gmail.com>
2022-06-15 16:24:42 +0100

    test 4 passed - Take Off a plane which is currently landed in airport and give a confirmation

--- src/airportRunner.js
+++ src/airportRunner.js
@@ -11,6 +11,12 @@ class Main {
         console.log(airport2.listOfLandedPlanes.length);
         airport2.listOfLandedPlanes = ['PLANE1', 'PLANE2'];
         console.log(airport2.landAPlane('PLANE3'));
+
+        let airport3 = new Airport;
+        console.log(airport3._capacity);
+        console.log(airport3.listOfLandedPlanes.length);
+        airport3.listOfLandedPlanes = ['PLANE1', 'PLANE2', 'PLANE3'];
+        console.log(airport3.takeOffPlane('PLANE1'));
     }
 }
 
