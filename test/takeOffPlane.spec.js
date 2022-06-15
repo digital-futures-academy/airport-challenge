f61761b7202100befbd943876526856197882a2b
pranay-a-1 <pranay.a4795@gmail.com>
2022-06-15 16:31:26 +0100

    test 5 passed - Prevent Take Off for a plane which is NOT currently landed in airport

--- test/takeOffPlane.spec.js
+++ test/takeOffPlane.spec.js
@@ -13,3 +13,11 @@ test.it('Take Off a plane which is currently landed in airport and give a confir
     test.assertEquals(airport.takeOffPlane(input), `${input} has Taken Off! and ${input} is no longer in the airport!`); //if listOfLandedPlanes includes PLANE1 test should pass
 });
 
+test.it('Prevent Take Off for a plane which is NOT currently landed in airport', () => {
+    // Arrange
+    let airport1 = new Airport(2);
+    let input = 'PLANE16';
+    airport1.listOfLandedPlanes = ['PLANE1', 'PLANE2']; // setting the 2 values to fill the capacity
+    // Act/Assert
+    test.assertEquals(airport1.takeOffPlane(input), `${input} cannot Take Off as it is not currently in landed list of planes in the airport`); //if listOfLandedPlanes includes PLANE1 test should pass
+});
