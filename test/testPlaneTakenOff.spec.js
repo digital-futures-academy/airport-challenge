690631eb41c8f18a779d7963c263e3abe51e63ed
Aroosa Bano Ahmed <aroosabanoahmed@gmail.com>
2022-06-16 20:11:20 +0100

    II: created planeTakeOff property in Plane class because it should be associated with each plane not with each airport

--- test/testPlaneTakenOff.spec.js
+++ test/testPlaneTakenOff.spec.js
@@ -10,7 +10,7 @@ let testName, actualOutput, expectedOutput, result, airport1, planeID;
 
 //arrange
 testName = `Test1: to check plane has taken off`;
-expectedOutput = `Plan has been landed`;
+expectedOutput = true;
 airport1 = new Airport(`ap002`, planeID, 5);
 planeID = new Plane(`pl004`);
 airport1.addPlaneInAirport(planeID);
@@ -22,12 +22,11 @@ planeID = new Plane(`pl009`);
 airport1.addPlaneInAirport(planeID);
 planeID = new Plane(`pl010`);
 airport1.addPlaneInAirport(planeID);
-planeID = new Plane(`pl005`);
-// airport1.setPlanesInAirport(planeID);
-// planeID = new Plane(`pl010`);
+// planeID = new Plane(`pl005`, false);
+
 
 //act
-actualOutput = airport1.landAPlane(planeID);
+actualOutput = airport1.planeTakeOff(planeID);
 
 //assert
 result = assertEquals(expectedOutput, actualOutput);
@@ -35,5 +34,6 @@ result = assertEquals(expectedOutput, actualOutput);
 //report
 console.log(`${testName}: ${result ? `Pass` : `Fail`}`);
 console.log(actualOutput);
-console.log(airport1.capacity);
+console.log(planeID.getHasPlaneTakenOff());
+
 // console.log(airport1.deletePlaneFromAirport());
