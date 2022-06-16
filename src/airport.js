78c168ebd92e96c5f35bda8d499d3bfc1b331253
Alistair Gibb <aligibb_uk@hotmail.com>
2022-06-16 19:45:51 +0100

    Add pseudocode to airport.js expected to be able to pass Test 1 of plane.spec.js

--- src/airport.js
+++ src/airport.js
@@ -1,27 +1,13 @@
-
-const { Plane } = require("./plane")
-
 class Airport {
-  // create class constructor
-  constructor() { };
 
+  // instructToLand pseudo logic:
+  // will accept a plane as an argument
+  // will call land() function of the plane.
+  // No return value specified as required in the domain model for requirement 1.
 
-  landPlane = (plane) => {
-    // create an outcome value that can track whether the landPlane function reached a successful or unsuccessful conclusion.
-    let outcome = ``;
-    // Check that the function has received a valid Plane object. Without a valid Plane object their will be nothing to instruct to land so the function must return an error. 
-    if (plane.constructor.name != "Plane") {
-      return 'Error: no Plane provided';
-    } else {
-      outcome = plane.land();   //call the land() function within the plane which will manage the plane's change of state to landed. Store the return of this function as an outcome so landPlane() can know whether the plane successfully landed.
-    }
-
-    return outcome === "plane landed" ? 'success' : 'failure'; //If the outcome of plane.land() was successful then return success, else return 'failure'
-  };
 
 }
 
 
-module.exports = {
-  Airport: Airport
-}
+module.exports = Airport;
+
