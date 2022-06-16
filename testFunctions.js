db8e255cdf4605d5c452749ac312ef99adde0c7b
Alistair Gibb <aligibb_uk@hotmail.com>
2022-06-16 14:47:56 +0100

    Update the testing framework to use a Test object with Test.it, Test.assertEquals, assertTrue and assertFalse functions

--- testFunctions.js
+++
@@ -1,9 +0,0 @@
-// Testing functions for export and use in tests e.g. function.spec.js
-
-// test comment on already staged file
-
-const assertEquals = (val1, val2) => val1 === val2;
-
-module.exports = {
-    assertEquals
-}
