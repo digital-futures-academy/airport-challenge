6e26b7cc40b340da676fcf33d01e5a093fc06b6d
Shafiul23 <shafiulmirza@hotmail.com>
2022-06-16 11:49:12 +0100

    airportSize removed and replaced with airportList.length.

--- src/airport2.js
+++ src/airport2.js
@@ -1,16 +1,15 @@
 class Airport2 {
     airportList = [];
-    capacity = 100;
-    airportSize = 99;
+    capacity = 3;
     weather = `clear`;
 
     landPlane = inputPlane => {
         const index1 = this.airportList.indexOf(inputPlane); //finds the index of the plane in the array
-        if (this.airportSize < this.capacity && index1 === -1 && this.weather === `clear`) { //if airport size isn't full and if plane isn't already in array
+        if (this.airportList.length < this.capacity && index1 === -1 && this.weather === `clear`) { //if airport size isn't full and if plane isn't already in array
             this.airportList.push(inputPlane); //then inputPlane will be pushed into the array
         } else if (index1 > -1) { //if the plane IS in the array then error returned
             return new Error(`This plane is already in the airport`)
-        } else if (this.capacity <= this.airportSize) { //if the airport is full then error is returned
+        } else if (this.capacity <= this.airportList.length) { //if the airport is full then error is returned
             return new Error(`Airport is full`)
         } else if (this.weather === `stormy`) {
             return new Error(`Weather is too stormy for landing`)
--- test/airport2.spec.js
+++ test/airport2.spec.js
@@ -9,7 +9,7 @@ let expectedOutput, actualOutput, result, input, testName;
 
 //Requirement 1 
 //Test 1
-console.log(`===Airport Test 1===`)
+//console.log(`===Airport Test 1===`)
 
 //arrange
 testName = `Test 1 - confirm plane has landed by testing if airportList array has a length greater than 0 `
@@ -30,7 +30,7 @@ console.log(`${testName}: ${result ? `PASS` : `FAIL`}`);
 
 //Requirement 2 
 //Test 2
-console.log(`===Airport Test 2===`)
+//console.log(`===Airport Test 2===`)
 testName = `Test 2 - return changed capacity for new airport class`
 input = `plane1`
 testAirport = new Airport2();
@@ -42,19 +42,23 @@ console.log(`${testName}: ${result ? `PASS` : `FAIL`}`);
 
 //Requirement 3
 //Test 3
-console.log(`===Airport Test 3===`)
+//console.log(`===Airport Test 3===`)
 testName = `Test 3 - return error when plane attmepts to land with a full airport`
-input = `plane1`
+input1 = `plane1`
+input2 = `plane2`
+input3 = `plane3`
+input4 = `plane4`
 testAirport = new Airport2()
-testAirport.setAirportSize(100);
-testAirport.landPlane(input);
-actualResult = testAirport.landPlane();
+testAirport.landPlane(input1);
+testAirport.landPlane(input2);
+testAirport.landPlane(input3);
+actualResult = testAirport.landPlane(input4);
 result = assertTrue(actualResult instanceof Error)
 console.log(`${testName}: ${result ? `PASS` : `FAIL`}`);
 
 //Requirement 4
 //Test 4
-console.log(`===Airport Test 4===`)
+//console.log(`===Airport Test 4===`)
 testName = `Test 4 - Call a method that removes the last landed plane from the airport and then confirm that it is no longer in the airportList array`
 input = `plane1`
 testAirport = new Airport2()
@@ -67,7 +71,7 @@ console.log(`${testName}: ${result ? `PASS` : `FAIL`}`);
 
 //Requirement 5
 //Test 5
-console.log(`===Airport Test 5===`)
+//console.log(`===Airport Test 5===`)
 testName = `Test 5 - Receive an error when attempting to take-off a plane that is not in airportList`
 input = `plane1`
 testAirport = new Airport2()
@@ -76,7 +80,7 @@ result = assertTrue(actualResult instanceof Error)
 console.log(`${testName}: ${result ? `PASS` : `FAIL`}`);
 
 //Test 6 
-console.log(`===Airport Test 6===`)
+//console.log(`===Airport Test 6===`)
 testName = `Test 6 - Receive an error when attempting to land a plane that is already in airportList`
 input = `plane1`
 testAirport = new Airport2()
@@ -88,7 +92,7 @@ console.log(`${testName}: ${result ? `PASS` : `FAIL`}`);
 
 //Requirement 6
 //Test  7 
-console.log(`===Airport Test 7===`)
+//console.log(`===Airport Test 7===`)
 testName = `Test 7 - Receive an error when attempting to take-off plane in stormy weather`
 input = `plane1`
 testAirport = new Airport2()
@@ -102,7 +106,7 @@ console.log(`${testName}: ${result ? `PASS` : `FAIL`}`);
 
 //Requirement 7
 //Test  8 
-console.log(`===Airport Test 8===`)
+//console.log(`===Airport Test 8===`)
 testName = `Test 8 - Receive an error when attempting to land plane in stormy weather`
 input = `plane1`
 testAirport = new Airport2()
@@ -114,7 +118,7 @@ console.log(`${testName}: ${result ? `PASS` : `FAIL`}`);
 
 //Requirement 8
 //Test  9
-console.log(`===Airport Test 9===`)
+//console.log(`===Airport Test 9===`)
 testName = `Return a number for the number of planes that have landed in the airport`
 input = `plane1`
 input2 = `plane2`
