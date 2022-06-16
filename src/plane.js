82c00644c0939063adc0c5c7f8dd58551a282fe2
Alistair Gibb <aligibb_uk@hotmail.com>
2022-06-16 15:58:35 +0100

    Pass Test 1 - plane.spec.js

--- src/plane.js
+++ src/plane.js
@@ -1,7 +1,15 @@
 class Plane {
-    constructor() {
+    //Private variable that will store the state of the plane. E.g. recording whether it has taken off or landed.
+    //initializes to blank string.
+    #state = '';
 
+    //As state is a private variable a getter is needed to see what its value is.
+    // Simply returns the #state variable.
+    getState() {
+        return this.#state;
     }
+
+
 
 }
 
