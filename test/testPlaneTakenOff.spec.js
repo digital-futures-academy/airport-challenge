fbf2e628b10df99c0520001789bfc66ffceaf9b8
Aroosa Bano Ahmed <aroosabanoahmed@gmail.com>
2022-06-16 14:20:44 +0100

    II: pass test for plane take off when it have not

--- test/testPlaneTakenOff.spec.js
+++ test/testPlaneTakenOff.spec.js
@@ -10,7 +10,7 @@ let testName, actualOutput, expectedOutput, result, airport1, planeID;
 
 //arrange
 testName = `Test1: to check plan has taken off`;
-expectedOutput = `yes plan has taken off`;
+expectedOutput = false;
 planeID = new Plane(`pl004`);
 airport1 = new Airport(`ap002`, planeID, 5);
 
