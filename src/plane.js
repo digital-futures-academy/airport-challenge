25ae0dda6fece4d1d6537ceb2064d78a19853822
Alistair Gibb <aligibb_uk@hotmail.com>
2022-06-17 12:53:42 +0100

    Pass Test 3 of plane.spec.js

--- src/plane.js
+++ src/plane.js
@@ -2,6 +2,13 @@ class Plane {
     //Private variable that will store the state of the plane. E.g. recording whether it has taken off or landed.
     //initializes to blank string.
     #state = '';
+    #planeId = 'noID';
+
+
+    getPlaneId() {
+        return this.#planeId;
+    }
+
 
     //As state is a private variable a getter is needed to see what its value is.
     // Simply returns the #state variable.
