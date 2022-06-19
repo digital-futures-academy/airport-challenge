aaeec675543e8159db8960cae0bdefc243812392
Monaire-p <monairepillay@gmail.com>
2022-06-19 22:53:44 +0100

    added changes to the first user story

--- src/airport.js
+++ src/airport.js
@@ -1,12 +1,40 @@
 class Airport {
-    #planeLanded = true; //property used to make the plane flight true and the # is used to make the flight private  
+    planeLanded = true;
+    #plane0 = true;
+    addPlaneId = true;
 
-    // here's a starting point for you
     planeLand() {
-        //method used to get a return of true for the plane being in flight     
-        return this.#planeLanded;
+
+        return this.planeLanded;
 
     }
+
+    planeLandId() {
+        return this.#plane0;
+    }
+
+    addPlaneId() {
+        return this.listOfIds;
+    }
+
+    constructor(planeID) {
+        this.planeID = planeID;
+    }
+}
+{
+
+    let mainAirport = [];
+
+    const plane3 = new Airport(`plane3`);
+    const plane4 = new Airport(`plane4`);
+    const plane5 = new Airport(`plane5`);
+
+    mainAirport = [plane3, plane4, plane5];
+
+    for (let i = 0; i < mainAirport.length; i++)
+        console.log(mainAirport[i].planeID);
+
+
 }
 
 module.exports = Airport;
--- src/plane.js
+++ src/plane.js
@@ -20,5 +20,3 @@ console.log(totalPlanes);
 
 module.exports = Plane;
 
-
-
