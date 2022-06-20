c912fefd76cad2b5239a74d95aa9a7e772ba068f
PaulMuigai <paulmuigai19@gmail.com>
2022-06-21 00:06:25 +0100

    Updated

--- test/airport.spec.js
+++ test/airport.spec.js
@@ -1,5 +1,6 @@
 const { assertEquals, assertTrue } = require("../test-framework");
 const { Airport } = require("../src/airport.js");
+const { Plane } = require("../src/plane.js");
 
 let testName, expectedResult, actualResult, result;
 
@@ -15,5 +16,23 @@ actualResult = airport1.planeList;
 
 //Assert 
 result = assertEquals(expectedResult[0], actualResult[0]);
+
+console.log(`${testName}: ${result ? 'pass' : 'fail'}`);
+
+
+
+//Test 2
+//Arrange 
+testName = 2
+expectedResult = 1;
+const airport2 = new Airport;
+const plane1 = new Plane;
+
+//Act 
+airport2.landPlane(plane1);
+actualResult = airport2.planeList.length;
+
+//Assert 
+result = assertEquals(expectedResult, actualResult);
 
 console.log(`${testName}: ${result ? 'pass' : 'fail'}`);
