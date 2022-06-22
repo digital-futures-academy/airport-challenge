3c3abbd17a8a1a2b5c17da1fc55931faa38ad0bc
PaulMuigai <paulmuigai19@gmail.com>
2022-06-22 08:34:32 +0100

    user story 2

--- test/airport.spec.js
+++ test/airport.spec.js
@@ -6,7 +6,7 @@ let testName, expectedResult, actualResult, result;
 
 //Test 1
 //Arrange 
-testName = 1
+testName = 1;
 expectedResult = [];
 const airport1 = new Airport;
 
@@ -23,7 +23,7 @@ console.log(`${testName}: ${result ? 'pass' : 'fail'}`);
 
 //Test 2
 //Arrange 
-testName = 2
+testName = 2;
 expectedResult = 1;
 const airport2 = new Airport;
 const plane1 = new Plane;
@@ -31,6 +31,22 @@ const plane1 = new Plane;
 //Act 
 airport2.landPlane(plane1);
 actualResult = airport2.planeList.length;
+
+//Assert 
+result = assertEquals(expectedResult, actualResult);
+
+console.log(`${testName}: ${result ? 'pass' : 'fail'}`);
+
+
+//Test 3 (when new airport instance is created, it has a default capacity value of 50 assigned to it)
+//Arrange 
+testName = 3;
+expectedResult = 50;
+const airport3 = new Airport();
+
+//Act 
+airport3.capacity;
+actualResult = airport3.capacity;
 
 //Assert 
 result = assertEquals(expectedResult, actualResult);
