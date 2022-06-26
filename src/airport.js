148b2b82292d28df86af76be987d72000c33f414
PaulMuigai <paulmuigai19@gmail.com>
2022-06-26 19:31:28 +0100

    Test 3 pass

--- src/airport.js
+++ src/airport.js
@@ -4,6 +4,9 @@ class Airport {
 
   landPlane(plane) {
     this.planesAtAirport.push(plane);
+    console.log(`planesAtAirport length: ${this.planesAtAirport.length}`);
+    console.log(this.planesAtAirport);
+    return `Confirming that ${plane} has landed`;
   }
 
 }
--- tests/airport.spec.js
+++ tests/airport.spec.js
@@ -1,4 +1,4 @@
-const { assertEquals } = require("../testFramework");
+const { assertEquals, assertTrue } = require("../testFramework");
 const Airport = require("../src/airport");
 
 // Fixtures
@@ -16,5 +16,31 @@ testAirport.landPlane(testPlane);
 actualResult = testAirport.planesAtAirport.length;
 // Assert 
 result = assertEquals(expectedResult, actualResult);
+//report 
+console.log(`${name}: ${result ? `PASS` : `FAIL`}`);
+
+
+// === landPlane tests ===
+name = `landPlane Test 2 - plane can be found in planesAtAirportArray`;
+console.log(`=== ${name} ===`);
+// Arrange 
+testAirport = new Airport();
+// Act
+testAirport.landPlane(testPlane);
+// Assert 
+result = assertTrue(testAirport.planesAtAirport.includes(testPlane));
+//report 
+console.log(`${name}: ${result ? `PASS` : `FAIL`}`);
+
+
+// === landPlane tests ===
+name = `landPlane Test 3 - landPlane returns string that confirms THE plane landed`;
+console.log(`=== ${name} ===`);
+// Arrange 
+testAirport = new Airport();
+// Act
+actualResult = testAirport.landPlane(testPlane);
+// Assert 
+result = assertTrue(actualResult.includes(testPlane));
 //report 
 console.log(`${name}: ${result ? `PASS` : `FAIL`}`);
