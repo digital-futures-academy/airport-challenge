e87ab742e72747f693d8b21bfa452f7cada26c31
Monaire Pillay <monairepillay@gmail.com>
2022-06-27 22:55:59 +0100

    test 3- passing

--- src/airport.js
+++ src/airport.js
@@ -5,6 +5,10 @@ class Airport {
 
  landPlane(plane) {
  this.planesAtAirport.push(plane);
+ console.log(`planesAtAirport length: ${this.planesAtAirport.length}`);
+ console.log(this.planesAtAirport);
+
+ return `confirmong that ${plane} has landed`;
 
   }
 
--- tests/airport.spec.js
+++ tests/airport.spec.js
@@ -26,27 +26,42 @@ result = assertEquals(expectedResult,actualResult);
 console.log(`${name}: ${result ? `PASS` : `FAIL`}`);
 
 
-name = `landPlane Test 2-item can be found in planesAtAirportArray`;
+name = `landPlane Test 2- plane can be found in planesAtAirportArray`;
 console.log(`==== ${name} ====`);
 
 //arrange
 //Create an airport for the test 
 testAirport = new Airport();
-///expectedResult = 1;
 
 //Act
 //call the method under test with the testPlane
 testAirport.landPlane(testPlane);
 
 
-
-//Assert that the length of the plansAtAirport array is now 1 (initially 0)
+//Assert that the string 'testPlane' is now included in the planes at airport array  
 result = assertTrue(testAirport.planesAtAirport.includes(testPlane));
 
 //report
 console.log(`${name}: ${result ? `PASS` : `FAIL`}`);
 
 
+name = `landPlane Test 3-landPlane returns string that confirms the plane landed`;
+console.log(`==== ${name} ====`);
+
+//arrange
+//Create an airport for the test 
+testAirport = new Airport();
+
+//Act
+//call the method under test with the testPlane
+actualResult = testAirport.landPlane(testPlane);
 
 
-//Assert that the string 'testPlane' is now included in the planes at airport array 
+//Assert that the string 'testPlane' is now included in the planes at airport array  
+result = assertTrue(actualResult.includes(testPlane));
+
+//report
+console.log(`${name}: ${result ? `PASS` : `FAIL`}`);
+
+
+
