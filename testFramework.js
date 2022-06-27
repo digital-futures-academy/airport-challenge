5f5ba742c8fa85f7d85c80ca0018c157b7983101
Oma Agbirigba <omaagb@gmail.com>
2022-06-27 23:33:54 +0100

    code along- test 2 pass

--- testFramework.js
+++ testFramework.js
@@ -1,4 +1,4 @@
 const assertEquals = (val1, val2) => val1 === val2;
-const assertTrue = val => val1 === true;
+const assertTrue = val1 => val1 === true;
 
-module.exports = {assertEquals}
+module.exports = { assertEquals, assertTrue };
--- tests/airport.spec.js
+++ tests/airport.spec.js
@@ -1,5 +1,6 @@
 const Airport = require("../src/airport");
 const { assertEquals } = require("../testFramework");
+const { assertTrue } = require("../testFramework");
 
 //Fixtures
 let testAirport, expectedResult, actualResult, result, name;
@@ -17,5 +18,17 @@ testAirport.landPlane(testPlane)
 actualResult = testAirport.planesAtAirport.length;
 // Assert
 result = assertEquals(expectedResult, actualResult);
+// Report
+console.log(`${name}: ${result ? `PASS` : `FAIL`}`);
+
+
+name = `landPlane Test 2 - plane can be found in planesAtAirport Array`
+console.log(`=== ${name} ===`);
+// Arrange
+testAirport = new Airport
+// Act
+testAirport.landPlane(testPlane);
+// Assert
+result = assertTrue(testAirport.planesAtAirport.includes(testPlane))
 // Report
 console.log(`${name}: ${result ? `PASS` : `FAIL`}`);
