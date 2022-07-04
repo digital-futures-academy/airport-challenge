7ccf0b1ff7145b78a496df7c894c013197eabdf6
Monaire Pillay <monairepillay@gmail.com>
2022-07-04 11:08:04 +0100

    changes to syntax

--- src/airport.js
+++ src/airport.js
@@ -27,24 +27,22 @@ class Airport {
   }
 
   isPlaneAtAirport(plane) {
-    if (this.planesAtAirport.includes(plane) === true); {
-
-
-    }
-
-
-    landPlane(plane) {
-      if (this.isAirportFull() === false) {
-        this.planesAtAirport.push(plane);
-        return `confirming that ${plane} has landed`;
-      }
-      return `airport is full`;
-
-    }
-
-
-
+    if (this.planesAtAirport.includes(plane) === true);
   }
+
+
+
+
+  landPlane(plane) {
+    if (this.isAirportFull() === false) {
+      this.planesAtAirport.push(plane);
+
+
+      return `confirming that ${plane} has landed`;
+    }
+    return `airport is full`;
+  }
+}
 
 module.exports = Airport;
 
