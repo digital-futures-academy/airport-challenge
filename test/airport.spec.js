bec94573e299c05d91035ca91d29df86db016dc5
Evie Ingold <evieingold@outlook.com>
2022-09-30 17:57:12 +0100

    Test 1b: add() adds a specific plane to the landedPlanes list

--- test/airport.spec.js
+++ test/airport.spec.js
@@ -17,11 +17,33 @@ actual = airport.landedPlanes.length;
 
 //Assert
 result = assertEquals(actual, expected);
-console.log(`Test 1: ${result}`);
+console.log(`Test 1a: ${result}`);
 
 //Clean-up
-airport = null;
-plane = null;
-expected = null;
-actual = null;
-result = null;
+airport = undefined;
+plane = undefined;
+expected = undefined;
+actual = undefined;
+result = undefined;
+
+console.log("Test 1b: landPlane() adds a specific plane to landedPlanes[]")
+
+//Arrange
+airport = new Airport();
+plane = new Plane();
+expected = plane;
+
+//Act
+airport.add(plane);
+actual = airport.landedPlanes[0];
+
+//Assert
+result = assertEquals(actual, expected);
+console.log(`Test 1b: ${result}`);
+
+//Clean-up
+airport = undefined;
+plane = undefined;
+expected = undefined;
+actual = undefined;
+result = undefined;
