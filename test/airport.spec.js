8ce021c5a81b2f9092dd2c3da19165e86cacd512
Deryn Cullen <info@deryncullen.com>
2022-10-02 15:25:33 +0100

    Story 3, test 1 of 4: failing test written

--- test/airport.spec.js
+++ test/airport.spec.js
@@ -14,7 +14,7 @@ let successMsg;
 
 // Test 1 - has the plane landed?
 
-console.log(`============================`);
+console.log(`===================================================`);
 console.log(
   `Test 1 - check success of landPlane: array length should change and success message should be logged`
 );
@@ -47,7 +47,7 @@ result = undefined;
 
 // Test 2 - Is there a default capacity on new airport instances and can it be changed?
 
-console.log(`============================`);
+console.log(`===================================================`);
 console.log(
   `Test 2 - check that airport capacity has a default value of 10 and is successfully changed with setCapacity`
 );
@@ -86,3 +86,57 @@ plane = null;
 expected = undefined;
 actual = undefined;
 result = undefined;
+
+// Test 3: Has airport capacity been reached? Are planes prevented from landing if it has?
+
+console.log(`===================================================`);
+console.log(
+  `Test 3 - check that airport capacity has been reached; check that planes cannot land if it has, and that appropriate messages are being returned to confirm`
+);
+
+// Arrange
+let plane1 = 'dfa001',
+  plane2 = 'dfa002',
+  plane3 = 'dfa003',
+  plane4 = 'dfa004',
+  plane5 = 'dfa005';
+
+myAirport = new Airport();
+myAirport.setCapacity(5);
+myAirport.landPlane(plane1);
+myAirport.landPlane(plane2);
+myAirport.landPlane(plane3);
+myAirport.landPlane(plane4);
+myAirport.landPlane(plane5);
+// console.log(myAirport.planeArray);
+expected = 'Airport capacity has been reached.';
+
+// Act
+actual = myAirport.landPlane;
+// Assert
+result = assertEquals(actual, expected);
+console.log(
+  `Test 3.1: Capacity reached message printed when capacity === planesArr.length: ${result}`
+);
+
+// Clean up
+myAirport = null;
+plane = null;
+expected = undefined;
+actual = undefined;
+result = undefined;
+
+// Arrange
+
+// Act
+
+// Assert
+
+// Clean up
+myAirport = null;
+plane = null;
+expected = undefined;
+actual = undefined;
+result = undefined;
+
+console.log(`===================================================`);
