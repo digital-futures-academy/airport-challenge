10d1f0f270d587cad5aa09d588dcd47fafe03e22
Sweta Shah <swetashah.8430@gmail.com>
2022-10-04 10:43:43 +0100

    change the framework file

--- testing-framework.js
+++ testing-framework.js
@@ -1,6 +1,14 @@
 const assertEquals = (actualResult, expectedResult) =>
   actualResult === expectedResult;
+const assertTrue = (actualResult) => actualResult === true;
+const assertFalse = (actualResult) => actualResult === false;
+function assertNull(actualResult) {
+  return actualResult === null;
+}
 
 module.exports = {
   assertEquals,
+  assertTrue,
+  assertFalse,
+  assertNull,
 };
