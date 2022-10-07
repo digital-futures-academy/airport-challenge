921817b0ec3faa412a06262842d8f3218b35e69f
Habashi94 <56684714+Habashi94@users.noreply.github.com>
2022-10-07 10:19:08 +0100

    Testing the capacity of the airport

--- src/airport.js
+++ src/airport.js
@@ -1,5 +1,22 @@
 class Airport {
-  // here's a starting point for you
+  planesAtAirstrip = [];
+  capacity;
+
+  constructor(capacity = 3) {
+    this.capacity = capacity;
+
+  }
+
+
+  landplane = (plane) => {
+    this.planesAtAirstrip.push(plane)
+    return 'Plane has successfully landed at the airstrip'
+
+  }
+
+  changeCapacity = (number) => {
+    this.capacity = number
+  }
 
 }
 
