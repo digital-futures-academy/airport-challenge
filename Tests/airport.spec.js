bd8056f36a0a8991d16450d2af2376a534cf9845
Matt Jones <119697940+mattjones26987@users.noreply.github.com>
2023-01-13 16:02:15 +0000

    Update airport.spec.js

--- Tests/airport.spec.js
+++ Tests/airport.spec.js
@@ -7,16 +7,25 @@ let plane;
 let airport;
 
 //1st Test
-console.log(`--------------------`)
-console.log(`1. addtailNumber adds Plane to the Airport`)
+console.log('--------------------');
+console.log('1. addtailNumber adds Plane to the Airport');
 
 //Arrange
-
+airport = new Airport();
+plane = new Plane();
+expected = 1;
 
 //Act
-
+airport.addplane(tailNumber);
+actual = airport.slotsOccupied.length;
 
 //Assert
-
+result = testEquals(expected, actual);
+console.log(`Airports Occupied slots increased ${result}`);
 
 //Cleanup
+expected = undefined;
+actual = undefined;
+result = undefined;
+plane = undefined;
+airport = undefined;
