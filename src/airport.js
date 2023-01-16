ce2f1d74f7173f9a37cdf475c1a574baf57ae875
Wil Treasure <wiltreasure@gmail.com>
2023-01-16 08:58:56 +0000

    Test 10 - Remove a plane from planes Landed PASSED

--- src/airport.js
+++ src/airport.js
@@ -1,13 +1,22 @@
 const Plane = require(`./Plane`);
 
 class Airport {
-  
+    
     planesLanded = [];
     capacity = 10;
 
     landPlane = plane => {
-    if (plane instanceof Plane && this.planesLanded.length < this.capacity) this.planesLanded.push(plane);
+        if (plane instanceof Plane && this.planesLanded.length < this.capacity) this.planesLanded.push(plane);
     };
+
+    takeOff = plane => {
+        const indexOfPlane = this.planesLanded.findIndex(planeLanded => planeLanded.id === plane.id);
+        console.log(indexOfPlane);
+
+        //if (indexOfPlane) {
+            this.planesLanded.splice(indexOfPlane, 1);
+       // }
+    }
 
     changeCapacity = capacity => {
         if (typeof capacity === "number" && capacity > 0 && capacity % 1 === 0) this.capacity = capacity;
