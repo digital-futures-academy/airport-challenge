bfdf3de38505ce3e8956a358bd0f034b745cccdb
Molly Stemp <molly_stemp@yahoo.com>
2023-01-18 11:27:48 +0000

    Progress so far

--- src/airport.js
+++ src/airport.js
@@ -1,6 +1,43 @@
-class Airport {
-  // here's a starting point for you
+const Plane = require(`./plane`);
 
+class Airport {
+  constructor(capacity = 0) {
+    this.capacity = capacity;
+    this.airportPlanes = [''];
+  }
+
+  landPlane = plane => {
+    if (plane instanceof Plane) this.airportPlanes.push(plane);
+    return console.log(plane)
+  
 }
 
-module.exports = Airport;
+    
+  isFull() {
+    return this.airportPlanes.length >= this.capacity
+  }
+
+  TakeOff = plane => {
+    const indexOfPlaneInAirportPlanes = this.airportPlanes.findIndex(airportPlane => airportPlane.id === Plane.id);
+
+    if (indexOfPlaneInAirportPlanes > -1) {
+      this.airportPlanes.splice(indexOfPlaneInAirportPlanes, 1);
+    } return console.log("Plane is no longer at the airport!")
+  }
+
+  isLanded = plane => {
+    if (this.airportPlanes.includes(Plane) & this.landPlane(Plane)) {
+      this.airportPlanes.pop(Plane)
+    }
+    }
+  
+  
+
+}
+  
+
+
+module.exports =
+  Airport;
+
+ 
