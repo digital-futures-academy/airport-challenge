22fd3a513c4db6ccd3d6461a7f0d2a792069acbd
Elle Ordona <eto0507@hotmail.co.uk>
2023-01-19 18:42:40 +0000

    eslint format fixes

--- src/airport.js
+++ src/airport.js
@@ -1,24 +1,21 @@
 // Airport Class
-
 const Plane = require(`./plane.js`);
 
 class Airport {
-  
-  //* properties of Airport
-  groundedPlanes = []; //array for planes in the airport
 
-  //* constructor for airport
-  constructor(airportID = 'The Airport', airportCapacity = 10) {
+  //* properties and constructor for airport
+  constructor(airportID = 'The Airport', airportCapacity = 10,  groundedPlanes = []) {
     this.airportID = airportID;
     this.airportCapacity = airportCapacity;
-  }; // set default values for these properties and they can be set when creating the airport
+    this.groundedPlanes = groundedPlanes;
+  };// set default values for these properties and they can be set when creating the airport
 
   //* functions of airport
   isAirportFull() {
     return (this.airportCapacity === this.groundedPlanes.length); // true or false
   };
 
-  changeAirportCapacity = capacity => {
+  changeAirportCapacity(capacity) {
     if (typeof capacity === 'number' && capacity >= this.groundedPlanes.length) {
       this.airportCapacity = capacity; // changes the capacity of the airport only if the input is a number
       console.log(`The capacity of ${this.airportID} has changed to ${this.airportCapacity}.`); // tell the user command has changed the airport capacity
@@ -32,7 +29,7 @@ class Airport {
     return this.airportCapacity; // returns the value of airportCapacity
   };
 
-  planeExists = plane => {
+  planeExists(plane) {
     return (-1 < this.groundedPlanes.findIndex(airportPlane => airportPlane.planeID === plane.planeID)); // true or false
   };
 
@@ -44,7 +41,7 @@ class Airport {
     return planesAtAirport;
   }
 
-  landPlane = plane => {
+  landPlane(plane) {
     if (plane instanceof Plane && this.isAirportFull() === false && this.planeExists(plane) === false) {
       this.groundedPlanes.push(plane); // adds the plane to the groundedPlanes array if the input is an instance of Plane class
       console.log(`${this.airportID} is not full, ${plane.planeID} has landed. There are ${this.groundedPlanes.length} plane(s) at ${this.airportID}.`); // show message that the plane has landed
@@ -57,7 +54,7 @@ class Airport {
     };
   };
 
-  takeoffPlane = plane => {
+  takeoffPlane(plane) {
     if (plane instanceof Plane && this.planeExists(plane) === true) {
       this.groundedPlanes.splice(this.groundedPlanes.findIndex(airportPlane => airportPlane.planeID === plane.planeID), 1);
       console.log(`${plane.planeID} has taken off from ${this.airportID}.`)
