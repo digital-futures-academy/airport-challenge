ed602291af79dae4abe5d3eaa70774c5ef35d55f
Elle Ordona <eto0507@hotmail.co.uk>
2023-01-19 18:43:43 +0000

    eslint format fixes

--- src/airport.js
+++ src/airport.js
@@ -1,5 +1,5 @@
 // Airport Class
-const Plane = require(`./plane.js`);
+const Plane = require('./plane.js');
 
 class Airport {
 
@@ -20,7 +20,7 @@ class Airport {
             this.airportCapacity = capacity; // changes the capacity of the airport only if the input is a number
             console.log(`The capacity of ${this.airportID} has changed to ${this.airportCapacity}.`); // tell the user command has changed the airport capacity
         } else {
-            console.log(`Invalid input, ${this.airportID} capacity has not changed.`) // tell the user that the input is invalid and nothing has changed
+            console.log(`Invalid input, ${this.airportID} capacity has not changed.`); // tell the user that the input is invalid and nothing has changed
         }
     }
 
@@ -35,7 +35,7 @@ class Airport {
 
     getPlanesAtAirport() {
         const planesAtAirport = JSON.stringify(this.groundedPlanes.map(function (plane) {
-            return plane['planeID']
+            return plane['planeID'];
         }));
         console.log(`${[planesAtAirport]} are at ${this.airportID}`);
         return planesAtAirport;
@@ -46,22 +46,22 @@ class Airport {
             this.groundedPlanes.push(plane); // adds the plane to the groundedPlanes array if the input is an instance of Plane class
             console.log(`${this.airportID} is not full, ${plane.planeID} has landed. There are ${this.groundedPlanes.length} plane(s) at ${this.airportID}.`); // show message that the plane has landed
         } else if (plane instanceof Plane && this.isAirportFull() === true && this.planeExists(plane) === false) {
-            console.log(`${this.airportID} is full, ${plane.planeID} has not landed.`) // show a message that the plane has on landed
+            console.log(`${this.airportID} is full, ${plane.planeID} has not landed.`); // show a message that the plane has on landed
         } else if (this.planeExists(plane) === true) {
             console.log(`${plane.planeID} is already at ${this.airportID} and cannot land.`);
         } else {
-            console.log(`Invalid input, nothing has landed at ${this.airportID}.`) // message for any invalid inputs put into the system
+            console.log(`Invalid input, nothing has landed at ${this.airportID}.`); // message for any invalid inputs put into the system
         }
     }
 
     takeoffPlane(plane) {
         if (plane instanceof Plane && this.planeExists(plane) === true) {
             this.groundedPlanes.splice(this.groundedPlanes.findIndex(airportPlane => airportPlane.planeID === plane.planeID), 1);
-            console.log(`${plane.planeID} has taken off from ${this.airportID}.`)
+            console.log(`${plane.planeID} has taken off from ${this.airportID}.`);
         } else if (plane instanceof Plane && this.planeExists(plane) === false) {
-            console.log(`${plane.planeID} is not at ${this.airportID} and cannot take off.`)
+            console.log(`${plane.planeID} is not at ${this.airportID} and cannot take off.`);
         } else {
-            console.log(`Invalid input, nothing has taken off from ${this.airportID}.`) // message for any invalid inputs put into the system
+            console.log(`Invalid input, nothing has taken off from ${this.airportID}.`); // message for any invalid inputs put into the system
         }
     }
   
