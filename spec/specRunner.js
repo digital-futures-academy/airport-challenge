5c2cce9e35cedbc9f12b104f8ce4383f9accfd78
Molly Stemp <molly_stemp@yahoo.com>
2023-01-20 11:01:14 +0000

    SpecRunner update from spec folder

---
+++ spec/specRunner.js
@@ -0,0 +1,10 @@
+const Airport = require(`./airport.spec`);
+const Plane = require(`../src/plane`);
+const Weather = require(`./weather.spec`)
+
+
+
+
+
+
+
--- spec/weather.spec.js
+++ spec/weather.spec.js
@@ -7,16 +7,15 @@ console.log(`=======================`)
 console.log(`Test 1: To check if when weather is called, it gives a "Stormy" or "Sunny" value. `)
 
 //Arrange 
-airport = new Airport();
 let weather = new Weather();
 expected1 = "Stormy";
 expected2 = "Calm";
 //Act 
-Weather.getWeather
-actual = this.weather
+weather.checkWeather();
+actual = weather.getWeather();
 
 //Assert
-result = assertEquals(actual, expected1) || assertEquals(actual, expected2)
+result = assertEquals(actual, expected2) || assertEquals(actual, expected1);
 console.log(`Test 1: To check if when weather is called, it gives a "Stormy" or "Sunny" value: ${result}`)
 
 //Clean up 
