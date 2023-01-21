9c4286a9b703711e35802774f3b1e15fd5f75c91
dpak33 <105285976+dpak33@users.noreply.github.com>
2023-01-21 09:12:50 +0000

    Update Airport.js

--- src/Airport.js
+++ src/Airport.js
@@ -8,15 +8,16 @@ class Airport {
 
     capacity = 10;
     airportPlanes = [];
+    allLandedPlanes = [];
 
 
 
 
     landPlane = (weather, plane) => {
-        if (!this.airportPlanes.includes(plane) && this.airportPlanes.length < this.capacity && weather < 0.85 && plane instanceof Airplane && plane.currentAirport == false) {
+        if (!this.airportPlanes.includes(plane) && this.airportPlanes.length < this.capacity && weather < 0.85 && plane instanceof Airplane && !this.allLandedPlanes.includes(plane)) {
             console.log("Plane landed!");
             this.airportPlanes.push(plane);
-            plane.currentAirport = this;
+            this.allLandedPlanes.push(plane);
         }
         else {
             console.log("A plane with that ID has just left the airport or the weather is stormy or the plane is stationed elsewhere or you are not flying a plane.")
@@ -29,6 +30,7 @@ class Airport {
         if (this.airportPlanes.includes(plane) && weather < 0.85) {
             console.log("Plane taken flight");
             this.airportPlanes.shift(plane);
+            this.allLandedPlanes.shift(plane);
         }
         else {
             console.log("A plane with that ID is not currently in the airport or the weather is stormy or you are not flying a plane");
