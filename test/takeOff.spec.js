1c7fa55726e5d59de173252fca2072573b7f8063
Frederik Robinson <frederik.l.robinson@gmail.com>
2023-06-09 22:30:37 +0100

    takeOff Test 4

--- test/takeOff.spec.js
+++ test/takeOff.spec.js
@@ -53,3 +53,51 @@ console.log(`Test 2: ${result ? "Pass" : "Fail"}`);
 airport.listOfPlanes = [];
 result = undefined;
 actual = undefined;
+
+console.log(`------------------`);
+
+//Test
+console.log(`Test 3: takeOff doesn't reduce the length of listOfPlanes if the plane isn't there`);
+
+//Arrange
+airport.listOfPlanes = [plane1];
+expected = 1;
+
+//Act
+airport.takeOff(plane2);
+actual = airport.listOfPlanes.length;
+
+//Assert
+result = assertEquals(actual, expected);
+
+//Report
+console.log(`Test 3: ${result ? "Pass" : "Fail"}`);
+
+//Cleanup
+airport.listOfPlanes = [];
+result = undefined;
+actual = undefined;
+
+console.log(`------------------`);
+
+//Test
+console.log(`Test 4: takeOff doesn't reduce the length of listOfPlanes if the input isn't a plane`);
+
+//Arrange
+airport.listOfPlanes = [plane1, plane2];
+expected = 2;
+
+//Act
+airport.takeOff("Not a plane");
+actual = airport.listOfPlanes.length;
+
+//Assert
+result = assertEquals(actual, expected);
+
+//Report
+console.log(`Test 4: ${result ? "Pass" : "Fail"}`);
+
+//Cleanup
+airport.listOfPlanes = [];
+result = undefined;
+actual = undefined;
