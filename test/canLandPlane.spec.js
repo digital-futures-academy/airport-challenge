9e4ec022f8d5436253006f15966b5d7ef71d6e8e
Wilfrick <27958603+Wilfrick@users.noreply.github.com>
2023-06-09 14:28:59 +0100

    canLandPlanes tests 1-6 passed

--- test/canLandPlane.spec.js
+++ test/canLandPlane.spec.js
@@ -16,5 +16,14 @@ const { Airport } = require("../src/airport.js");
 
     test(airport.planesOnGround[0]).equals(emptyPlane);
     test(airport.planesOnGround[airport.planesOnGround.length - 1]).equals(planeWithCallsignAndDest);
+
+    test(airport.planesOnGround.length).equals(2);
+
+    airport.planesOnGround = [];
+    for (let i = 0; i < 1000; i++) {
+        airport.landPlane(planeWithCallsignAndDest);
+    }
+    test(airport.planesOnGround.length).equals(1000);
+
     test.showSummary();
 }
