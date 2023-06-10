18bf66f6fcf0a7597f9b36c0307f240dfb04d60e
SaraAGithub-Portfolio <skausa29@gmail.com>
2023-06-10 12:28:38 +0100

    Updated file for formatting issues

--- specs/airport.spec.js
+++ specs/airport.spec.js
@@ -9,14 +9,14 @@ console.log(`Test 1`);
 console.log(``);
 
 console.log(` Testing if airport was instructed to land the plane using airport.land(plane)`);
-//Arrange
+
 expected = true;
 plane = {};
-//Act
+
 result = airport.land(plane);
 actual = airport.destination.includes(plane);
-//Assert
+
 result = assertEquals(actual, expected);
-//Report
+
 console.log(`Test1: plane landed: ${result}`);
 console.log(`Test 1: ${result ? `PASS` : `FAIL`}`);
