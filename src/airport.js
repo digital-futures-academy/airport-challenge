a43249112ca98c27ede8cff961008c6886ca5fc6
KeenanMR <keenan.r.michael@gmail.com>
2023-06-10 15:22:03 +0100

    Story 1 Test 2 Added

--- src/airport.js
+++ src/airport.js
@@ -2,10 +2,7 @@ class Airport {
   constructor() {
     this.planesAtAirport = [];
   }
-
-
   landPlane = (plane) => { this.planesAtAirport.push(plane) };
-
 }
 
 class Plane {
--- test/airport.spec.js
+++ test/airport.spec.js
@@ -21,10 +21,41 @@ Test = () => {
     airport.landPlane(input);
     actual = airport.planesAtAirport.length;
 
-    result = assertStrictEqual(expected, actual);
+
     //Assert
+    result = assertStrictEqual(expected, actual);
 
     console.log(chalk.cyan(`Test 1 Result: ${result ? chalk.green(`PASS`) : chalk.red(`FAIL`)}`))
+    console.log(chalk.cyan(`=====================================================================================================`))
+    console.log(``);
+
+    //Cleanup
+    input = undefined;
+    expected = undefined;
+
+}
+
+//Story 1 Test 2 - Check the added item is a plane
+Test = () => {
+
+    console.log(chalk.cyan(`User Story 1: Land a plane at the airport`))
+    console.log(chalk.cyan(`User Story 1 Test 2: Add a plane object to an array within the airport and check that it is a Plane.`))
+    console.log(chalk.cyan(`User Story 1 Pass Criteria: The object in the array should have an ID of Plane1`))
+    console.log(``);
+
+    //Arrange
+    expected = `Plane1`;
+    input = new Plane();
+    airport = new Airport();
+    airport.landPlane(input);
+
+    //Act
+    actual = airport.planesAtAirport[0].getID();
+
+    //Assert
+    result = assertStrictEqual(expected, actual);
+
+    console.log(chalk.cyan(`Test 2 Result: ${result ? chalk.green(`PASS`) : chalk.red(`FAIL`)}`))
     console.log(chalk.cyan(`=====================================================================================================`))
     console.log(``);
 
