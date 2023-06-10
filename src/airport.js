8cceb406a31983b3ad11e2502502a0b2cbd804f0
SaraAGithub-Portfolio <skausa29@gmail.com>
2023-06-10 12:33:43 +0100

    added production code after failing/passing test. Land method receives plane parameter; method returns true/false to show successful landing.

--- src/airport.js
+++ src/airport.js
@@ -1,1 +1,14 @@
+const airport = {
+    land: function (plane) {
+        this.destination.push(plane);
+        return true;
+    },
+    isAtAirport: function (plane) {
+        return this.destination.includes(plane);
+    }
+};
+
+module.exports = airport;
+
+
 
