89d9178d105f813e2d08d3d9a4ae98abf75c19af
KeenanMR <keenan.r.michael@gmail.com>
2023-06-10 17:45:54 +0100

    Story 4 Test 1 Implemented

--- specRunner.js
+++ specRunner.js
@@ -2,4 +2,5 @@ const chalk = require("chalk");
 
 require(`./test/airportStory1.spec`);
 require(`./test/airportStory2.spec`);
-require(`./test/airportStory3.spec`); 
+require(`./test/airportStory3.spec`);
+require(`./test/airportStory4.spec`); 
---
+++ test/airportStory4.spec.js
@@ -0,0 +1,42 @@
+const chalk = require("chalk");
+const { assertStrictEqual } = require("./test-framework");
+const { Airport, Plane } = require("../src/airport");
+
+let input, result, actual, expected;
+
+//Story 3 Test - Takeoff a plane and confirm it has left
+Test = () => {
+
+    console.log(chalk.cyan(`User Story 4: Takeoff a plane and confirm it has left`))
+    console.log(chalk.cyan(`User Story 4 Test 1: Takeoff a plane`))
+    console.log(chalk.cyan(`User Story 4 Pass Criteria: The landed planes array should decrease to 1`))
+    console.log(``);
+
+    //Arrange
+    expected = 1;
+    input = 1;
+    plane = new Plane(`Plane`);
+    airport = new Airport(2);
+    for (let i = 0; i < 2; i++) {
+        airport.landPlane(plane);
+    }
+
+    //Act
+    airport.takeoffPlane(input);
+    actual = airport.planesAtAirport.length;
+
+    //Assert
+    result = assertStrictEqual(expected, actual);
+
+    console.log(chalk.cyan(`User Story 3 Test 1 Result: ${result ? chalk.green(`PASS`) : chalk.red(`FAIL`)}`))
+    console.log(chalk.cyan(`=====================================================================================================`))
+    console.log(``);
+
+    //Cleanup
+    input = undefined;
+    expected = undefined;
+
+}
+
+
+Test();
