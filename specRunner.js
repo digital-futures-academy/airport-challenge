ec1ec79a35dcf60e5e611031db6b38c1f7363f09
KeenanMR <keenan.r.michael@gmail.com>
2023-06-10 15:46:08 +0100

    Added Story 3 Test 1

--- specRunner.js
+++ specRunner.js
@@ -1,4 +1,5 @@
 const chalk = require("chalk");
 
 require(`./test/airportStory1.spec`);
-require(`./test/airportStory2.spec`); 
+require(`./test/airportStory2.spec`);
+require(`./test/airportStory3.spec`); 
---
+++ test/airportStory3.spec.js
@@ -0,0 +1,41 @@
+const chalk = require("chalk");
+const { assertStrictEqual } = require("./test-framework");
+const { Airport, Plane } = require("../src/airport");
+
+let input, result, actual, expected;
+
+//Story 3 Test - Prevent landing at a full airport
+Test = () => {
+
+    console.log(chalk.cyan(`User Story 3: Prevent landing at a full airport`))
+    console.log(chalk.cyan(`User Story 3 Test 1: Prevent a plane being added to the airport array if it is at capacity`))
+    console.log(chalk.cyan(`User Story 3 Pass Criteria: The landed planes array should remain at 2`))
+    console.log(``);
+
+    //Arrange
+    expected = 2;
+    input = new Plane(`Plane`);
+    airport = new Airport(2);
+    for (let i = 0; i < 2; i++) {
+        airport.landPlane(input);
+    }
+
+    //Act
+    airport.landPlane(input);
+    actual = airport.getPlanesAtAirport.length;
+
+    //Assert
+    result = assertStrictEqual(expected, actual);
+
+    console.log(chalk.cyan(`Test 1 Result: ${result ? chalk.green(`PASS`) : chalk.red(`FAIL`)}`))
+    console.log(chalk.cyan(`=====================================================================================================`))
+    console.log(``);
+
+    //Cleanup
+    input = undefined;
+    expected = undefined;
+
+}
+
+
+Test();
