027db05f08d6af688a971a9b86056fd98442f379
KeenanMR <keenan.r.michael@gmail.com>
2023-06-11 00:47:36 +0100

    Story 4 Test 2

--- test/airportStory4.spec.js
+++ test/airportStory4.spec.js
@@ -4,7 +4,7 @@ const { Airport, Plane } = require("../src/airport");
 
 let input, result, actual, expected;
 
-//Story 3 Test - Takeoff a plane and confirm it has left
+//Story 4 Test - Takeoff a plane and confirm it has left
 Test = () => {
 
     console.log(chalk.cyan(`User Story 4: Takeoff a plane and confirm it has left`))
@@ -38,5 +38,40 @@ Test = () => {
 
 }
 
+Test2 = () => {
+
+    console.log(chalk.cyan(`User Story 4: Takeoff a plane and confirm it has left`))
+    console.log(chalk.cyan(`User Story 4 Test 1: Takeoff a plane and confirm the plane that left`))
+    console.log(chalk.cyan(`User Story 4 Pass Criteria: The plane with the correct ID should be removed`))
+    console.log(``);
+
+    //Arrange
+    expected = `Airtrain`;
+    input = `BrainAir`;
+    plane1 = new Plane(`BrianAir`);
+    plane2 = new Plane(`Airtrian`);
+    airport = new Airport(2);
+    airport.landPlane(plane1);
+    airport.landPlane(plane2);
+
+
+    //Act
+    airport.takeoffPlane(input);
+    actual = airport.planesAtAirport[0].getID;
+
+    //Assert
+    result = assertStrictEqual(expected, actual);
+
+    console.log(chalk.cyan(`User Story 4 Test 2 Result: ${result ? chalk.green(`PASS`) : chalk.red(`FAIL`)}`))
+    console.log(chalk.cyan(`=====================================================================================================`))
+    console.log(``);
+
+    //Cleanup
+    input = undefined;
+    expected = undefined;
+
+}
+
 
 Test();
+Test2();
