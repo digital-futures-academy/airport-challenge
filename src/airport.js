1ea166a5aefd3568bee060165fcea52dd772348c
SaraAGithub-Portfolio <skausa29@gmail.com>
2023-06-11 12:16:17 +0100

    updated airport.js file to include new isFull function.

--- src/airport.js
+++ src/airport.js
@@ -1,7 +1,19 @@
 const airport = {
+    airportDestination: [],
+    airportCapacity: 100,
+
     land: function (plane) {
         this.airportDestination.push(plane);
         return true;
+    },
+    isAtCapacity: function () {
+        if (this.airportCapacity > 100) { return true } else { return false };
+    },
+    setCapacity: function (capacity) {
+        this.airportCapacity = capacity;
+    },
+    isFull: function () {
+        return this.isAtCapacity();
     },
 };
 
