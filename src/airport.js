cf673dcec5b45d7ac6eb8811d24842222b2eb476
Oana <oana.demian@gmail.com>
2023-06-12 11:29:23 +0100

    checks aircraftId and aircraftStatus are defined

--- src/airport.js
+++ src/airport.js
@@ -5,7 +5,7 @@ export default class Airport {
     this.maxAirportCapacity = 10;
   }
   landPlane(plane) { 
-    if (plane.aircraftId && !this.isFull()) {
+    if (plane?.aircraftId && plane?.aircraftStatus && !this.isFull()) {
       this.landedPlanes = [...this.landedPlanes, plane];
       plane.aircraftStatus = 'landed';
     }
