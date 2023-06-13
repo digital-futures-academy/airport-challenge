8713726cb03679b64620fa2f67edf798c2ff8867
SaraAGithub-Portfolio <skausa29@gmail.com>
2023-06-13 18:29:02 +0100

    Updated formatting of the plane.spec file to make the output in terminal more organized.

--- specs/plane.spec.js
+++ specs/plane.spec.js
@@ -1,17 +1,18 @@
-const { assertEquals } = require("./testing-framework");
-const plane = require("../src/plane");
+const { assertEquals } = require('./testing-framework');
+const plane = require('../src/plane');
 
 let actual, result, expected;
-
-console.log(`Test 2`);
-console.log(``);
-
+//Test 1
+console.log(`==============`);
+console.log(`Test 1 Plane Spec`);
 console.log(` Testing if plane is at airport using isAtAirport(plane)`);
 
+
 expected = true;
-actual = plane.isAtAirport(plane);
+actual = plane.isAtAirport();
 result = assertEquals(actual, expected);
 
-console.log(`Test 2: Plane has landed at airport ${result}`);
-console.log(`Test 2:${result ? `PASS` : `FAIL`}`);
+console.log(`Test 1 Plane Spec: Plane has landed at airport: ${result}`);
+console.log(`Test 1 Plane Spec:${result ? `PASS` : `FAIL`}`);
 console.log(``);
+
