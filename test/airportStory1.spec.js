3294e7a6dbfde93273d69bd40798a570bd2a2db3
KeenanMR <keenan.r.michael@gmail.com>
2023-06-14 14:55:08 +0100

    Update airportStory1.spec.js
    Added a new test

--- test/airportStory1.spec.js
+++ test/airportStory1.spec.js
@@ -5,12 +5,40 @@ const { Plane } = require("../src/Plane");
 
 
 let input, result, actual, expected;
-
 //Story 1 Test - Land a plane at an airport
 Test = () => {
 
     console.log(chalk.cyan(`User Story 1: Land a plane at the airport`))
-    console.log(chalk.cyan(`User Story 1 Test 1: Add a plane object to an array within the airport.`))
+    console.log(chalk.cyan(`User Story 1 Test 1: Check that a plane can land`))
+    console.log(chalk.cyan(`User Story 1 Pass Criteria: canLand() should return true`))
+    console.log(``);
+
+    //Arrange
+    expected = true;
+    input = new Plane();
+    airport = new Airport(5);
+
+    //Act
+    actual = airport.canLand();
+
+    //Assert
+    result = assertStrictEqual(expected, actual);
+
+    console.log(chalk.cyan(`User Story 1 Test 1 Result: ${result ? chalk.green(`PASS`) : chalk.red(`FAIL`)}`))
+    console.log(chalk.red(`=====================================================================================================`))
+    console.log(``);
+
+    //Cleanup
+    input = undefined;
+    expected = undefined;
+
+}
+
+//Story 1 Test - Land a plane at an airport
+Test2 = () => {
+
+    console.log(chalk.cyan(`User Story 1: Land a plane at the airport`))
+    console.log(chalk.cyan(`User Story 1 Test 2: Add a plane object to an array within the airport.`))
     console.log(chalk.cyan(`User Story 1 Pass Criteria: The plane array should have a length of 1`))
     console.log(``);
 
@@ -27,7 +55,7 @@ Test = () => {
     //Assert
     result = assertStrictEqual(expected, actual);
 
-    console.log(chalk.cyan(`User Story 1 Test 1 Result: ${result ? chalk.green(`PASS`) : chalk.red(`FAIL`)}`))
+    console.log(chalk.cyan(`User Story 1 Test 2 Result: ${result ? chalk.green(`PASS`) : chalk.red(`FAIL`)}`))
     console.log(chalk.red(`=====================================================================================================`))
     console.log(``);
 
@@ -38,10 +66,10 @@ Test = () => {
 }
 
 //Story 1 Test 2 - Check the added item is a plane
-Test2 = () => {
+Test3 = () => {
 
     console.log(chalk.cyan(`User Story 1: Land a plane at the airport`))
-    console.log(chalk.cyan(`User Story 1 Test 2: Add a plane object to an array within the airport and check that it is a Plane.`))
+    console.log(chalk.cyan(`User Story 1 Test 3: Add a plane object to an array within the airport and check that it is a Plane.`))
     console.log(chalk.cyan(`User Story 1 Pass Criteria: The object in the array should have an ID of Plane1`))
     console.log(``);
 
@@ -57,7 +85,7 @@ Test2 = () => {
     //Assert
     result = assertStrictEqual(expected, actual);
 
-    console.log(chalk.cyan(`User Story 1 Test 2 Result: ${result ? chalk.green(`PASS`) : chalk.red(`FAIL`)}`))
+    console.log(chalk.cyan(`User Story 1 Test 3 Result: ${result ? chalk.green(`PASS`) : chalk.red(`FAIL`)}`))
     console.log(chalk.red(`=====================================================================================================`))
     console.log(``);
 
@@ -69,3 +97,4 @@ Test2 = () => {
 
 Test();
 Test2();
+Test3();
