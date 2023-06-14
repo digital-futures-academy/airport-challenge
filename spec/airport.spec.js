6459a814bf5d497aad1b3df0aecc57a0782390e3
Chioma Onyeneke <ccchioma60@gmail.com>
2023-06-14 16:54:33 +0100

    test that landPlane increases planeList by 1

--- spec/airport.spec.js
+++ spec/airport.spec.js
@@ -1,7 +1,7 @@
 import Airport from '../src/Airport.js';
 
 describe('Airport Class Tests', () => {
-    let actual, airport, expected, plane;
+    let airport, plane;
 
     beforeEach(() => {
         airport = new Airport();
@@ -10,60 +10,21 @@ describe('Airport Class Tests', () => {
 
      afterEach(() => {
         airport = undefined;
-         actual = undefined;
-         expected = undefined;
      });
 
-    it('should call the landPlane on plane to land it into the planeList array in Airport and increase planeList by 1', () => {
+    it('Test that `landPlane` will land a `Plane` to the `planeList`, increasing its length by 1', () => {
         // Arrange
-        expected = 1;
         plane = 'plane1';
         airport = new Airport();
 
         // Act
         airport.landPlane(plane);
-        actual = airport.planeList.length;
 
         // Assert
-        expect(actual).toBe(expected);
+        expect(airport.planeList.length).toBe(1);
 
 
     });
 
-    it('should not land a plane of a different data type into the planeList', () => {
-        // Arrange
-        expected = 0;
-        plane = 10;
-
-        // Act
-        airport.landPlane(plane);
-        actual = airport.planeList.length;
-
-        // Assert
-        expect(actual).toBe(expected);
-    })
-
-    it('should have a defaultCapacity of 20', () => {
-        // Arrange
-        expected = 20;
-
-        // Act
-        actual = airport.defaultCapacity;
-
-        // Assert
-        expect(actual).toBe(expected);
-    })
-
-    it('should increase defaultCapacity when a positive number is passed to OverrideCapacity', () => {
-        // Arrange
-        let increaseCapacity = 5;
-        expected = 25;
-
-        // Act
-        actual = airport.overrideCapacity(increaseCapacity);
-
-        // Assert
-        expect(actual).toBe(expected);
-    });
 
 });
--- src/airport.js
+++ src/airport.js
@@ -1,18 +1,8 @@
 class Airport{
     planeList = [];
 
-    constructor() {
-        this.defaultCapacity = 20;
-    }
-
     landPlane(plane) {
-        if (typeof plane == 'string') this.planeList.push(plane);
-    }
-
-    overrideCapacity(number) {
-        if (number > 0) {
-            return this.defaultCapacity += number;
-        }
+        this.planeList = [...this.planeList, plane];
     }
 
 }
