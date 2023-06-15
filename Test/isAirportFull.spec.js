46bf8c6543c037648ac6200ae50e64b4757b112a
David Kilpatrick <Davidkilpatrick@outlook.com>
2023-06-15 19:23:11 +0100

    isfull test 3 complete - no source required

--- Test/isAirportFull.spec.js
+++ Test/isAirportFull.spec.js
@@ -63,4 +63,36 @@ result = undefined;
 
 console.log('----------------------------------');
 
+// Check if airport is full - change max capacity of airport to ensure isFull correctly reflecting change in limit. Change airport to 3 and land 2 planes
+
+//arrange
+
+airport = new Airport();
+plane1 = new Plane('A1')
+plane2 = new Plane('B2')
+
+expected = false
+
+//act
+
+airport.setAirportMaxCapacity(3)
+airport.instructToLand(plane1);
+airport.instructToLand(plane2);
+actual = airport.isAirportFull();
+
+//assert
+
+result = assertEquals(actual, expected);
+
+console.log(`Test 3 - IsAirportFull returns false after two plane landings as capacity is now 3 ${result ? `PASS` : `FAIL`}`);
+
+//cleanup
+
+input = undefined;
+expected = undefined;
+actual = undefined;
+result = undefined;
+
+console.log('----------------------------------');
+
 
