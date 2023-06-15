fe5d0cad591498823b007c7f10539fe4d666f5a1
Dileep <dileepvunnam@gmail.com>
2023-06-15 21:33:18 +0100

    2 planes at same time

--- test/preventLanding.spec.js
+++ test/preventLanding.spec.js
@@ -1,20 +1,38 @@
 const { assertEquals } = require("./testingFramework");
 const airport = require("../src/airport");
 let actualOutput, expectedOutput, result;
-const plane = { id: '9A06' };
+const plane1 = { id: '9A06' };
+const plane2 = { id: '3A05' };
 
-//User Story 3 - Test 1: Prevent Landing When the Airport is Full
+// Test 1: Prevent Landing When the Airport is Full
 console.log("User Story 3 - Test 1: Prevent Landing When the Airport is Full");
 
 // Arrange
 expectedOutput = false;
 
 // Act
-airport.landPlane(plane);
+airport.landPlane(plane1);
 actualOutput = airport.isFull();
 
 // Assert
 result = assertEquals(actualOutput, expectedOutput);
 console.log(`Test: Prevent landing when airport is full: ${result}`);
+console.log(`Test: ${result ? "PASS" : "FAIL"}`);
+console.log("");
+
+//Test 2: Prevent Landing When two planes are landing concurrently
+console.log("Test 2: Prevent Landing When two planes are landing concurrently");
+
+// Arrange
+airport.capacity = 1;
+expectedOutput = true;
+
+// Act
+airport.landPlane(plane2);
+actualOutput = airport.isFull();
+
+// Assert
+result = assertEquals(actualOutput, expectedOutput);
+console.log(`Test: Prevent Landing When two planes are landing concurrently: ${result}`);
 console.log(`Test: ${result ? "PASS" : "FAIL"}`);
 console.log("");
