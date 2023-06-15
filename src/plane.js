ab310e659aacb62b8dd19f3f8938d995866cde01
David Kilpatrick <Davidkilpatrick@outlook.com>
2023-06-15 16:40:01 +0100

    plane source code

---
+++ src/plane.js
@@ -0,0 +1,15 @@
+class Plane {
+
+    constructor(planeID) {
+        this.planeID = planeID
+    }
+
+    getPlaneID() {
+        return this.planeID
+    }
+
+}
+
+module.exports = {
+    Plane,
+}
