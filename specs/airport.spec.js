335daaae55a06b271635b36bb2358936c9e4f972
SaraAGithub-Portfolio <skausa29@gmail.com>
2023-06-16 16:56:06 +0100

    Changed some of the wording of the tests to make it more clear. Changed capacity value to smmaler number.

--- specs/airport.spec.js
+++ specs/airport.spec.js
@@ -1,75 +1,64 @@
-const { assertEquals } = require('./testing-framework');
-const airport = require('../src/airport');
-const plane = require('../src/plane');
+const { assertEquals } = require("./testing-framework");
+const airport = require("../src/airport");
+const plane = require("../src/plane");
 
 let actual, result, expected;
 
 airport.planeLanded = [];
-
 //Test 1
-console.log(`Test 1 Airport Spec`);
-console.log(``);
-
-console.log(` Testing if airport was instructed to land the plane)`);
+console.log(`Test 1: Airport was instructed to land the plane`);
 
 expected = true;
 actual = airport.land(plane);
 result = assertEquals(actual, expected);
 
-console.log(`Test 1 Airport Spec: plane landed: ${result}`);
-console.log(`Test 1 Airport Spec: ${result ? `PASS` : `FAIL`}`);
+console.log(`Test 1: plane landed: ${result}`);
+console.log(`Test 1: ${result ? `PASS` : `FAIL`}`);
 
 //Test 2
-console.log(`Test 2 Airport Spec`);
-console.log(``);
-console.log(`Test default airport capacity`);
+console.log(`==============`);
+console.log(`Test 2: Update default airport capacity`);
 
-expected = 150;
-airport.setMax(150);
-actual = airport.airportCapacity;
+expected = 4;
+airport.updateCapacity(4);
+actual = airport.defaultAirportCapacity;
 result = assertEquals(actual, expected);
 
-console.log(`Test 2 Airport Spec: test Airport capacity: ${result}`);
-console.log(`Test 2 Airport Spec: ${result ? `PASS` : `FAIL`}`);
+console.log(`Test 2: Airport capacity can be overridden: ${result}`);
+console.log(`Test 2: ${result ? `PASS` : `FAIL`}`);
 
 //Test 3
-console.log(`Test 3 Airport Spec`);
-console.log(``);
-console.log(`Test: Airport is full`);
-
-console.log(`Airport capacity: ${airport.airportCapacity}`);
-
-expected = false;
-actual = airport.isFull();
-result = assertEquals(actual, expected);
-console.log(`Test 3 Airport Spec: is Airport Full : ${result}`);
-console.log(`Test 3 Airport Spec: ${result ? `PASS` : `FAIL`}`);
-
-//Test 4
-console.log(`Test 4 Airport Spec`);
-console.log(``);
-console.log(`Test: Instruct Airport to let plane take off`);
+console.log(`==============`);
+console.log(`Test 3: Instruct Airport to let plane take off`);
 
 expected = true;
 actual = airport.takeOff(plane);
 result = assertEquals(actual, expected);
 
-console.log(`Test 4 Airport Spec: Instruct Airport to let plane takeoff: ${result}`);
-console.log(`Test 4 Airport Spec: ${result ? `PASS` : `FAIL`}`);
+console.log(`Test 3: Airport can instruct plane to take off: ${result}`);
+console.log(`Test 3: ${result ? `PASS` : `FAIL`}`);
 
+//Test 4
+console.log(`==============`);
+console.log(`Test 4: Prevent landing when airport is full`);
 
-//Test 5
-console.log(`Test 5 Airport Spec`);
-console.log(``);
-console.log(`Test: Prevent landing when airport is full`);
-console.log(`Airport capacity: ${airport.airportCapacity}`);
-
-for (let i = 0; i < airport.airportCapacity; i++) {
+for (let i = 0; i < airport.defaultAirportCapacity; i++) {
     airport.land(plane);
 }
 expected = false;
 actual = airport.land(plane);
 result = assertEquals(actual, expected);
 
-console.log(`Test 5 Airport Spec: Prevent landing when airport is full`);
-console.log(`Test 5 Airport Spec: ${result ? `PASS` : `FAIL`}`);
+console.log(`Test 4: Plane is prevented from landing because the airport is full: ${result}`);
+console.log(`Test 4: ${result ? `PASS` : `FAIL`}`);
+
+//Test 5
+console.log(`=============`);
+console.log(`Test 5: Prevent asking the airport to let a plane take off if it is not at the airport`);
+
+expected = true;
+actual = airport.takeOff(plane);
+result = assertEquals(actual, expected);
+console.log(`Test 5: Prevent airport instructing plane to take off when it is not at the airport: ${result}`);
+console.log(`Test 5: ${result ? `PASS` : `FAIL`}`);
+
