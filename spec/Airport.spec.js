d5545b9495cfcdcb6f86e3990847e5e67bf00b22
ifrashaikh550 <126068731+Ifra550@users.noreply.github.com>
2023-06-16 18:33:29 +0100

    User Story 2
    Working code with Airport capacity that can be overridden.

--- spec/Airport.spec.js
+++ spec/Airport.spec.js
@@ -8,4 +8,12 @@ describe('Test plane landing', () => {
         const actual = airport.add('plane1');
         expect(actual).toBe(1);
     });
+    it('Set capacity', () => {
+        // Arrange
+        // Act
+        // Assert
+    const airport = new Airport();
+    const actual = airport.overrideCapacity(5);
+    expect(actual).toBe(5);
+    });
 });
--- src/airport.js
+++ src/airport.js
@@ -1,12 +1,22 @@
 class Airport {
     constructor() {
      this.airport1 = []
+     this.capacity = 3  //Default capacity is 3
     }
 
     add(plane) {
-        this.airport1.push(plane);
-        console.log(this.airport1)
-        return this.airport1.length;
+        if (this.airport1.length < this.capacity)
+        {
+            this.airport1.push(plane);
+            //console.log(this.airport1)
+            return this.airport1.length;
+        }
+    }
+
+    overrideCapacity(limit)
+    {
+        this.capacity = limit
+        return this.capacity
     }
 }
 export default Airport;
