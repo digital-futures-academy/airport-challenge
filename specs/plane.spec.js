c73e301ba10f2058d815f0fe35d5f4f239c2503c
SaraAGithub-Portfolio <skausa29@gmail.com>
2023-06-16 16:57:35 +0100

    Changed wording of the tests to make it more clear. Added additional test(after it failed, then passed) to test if planes already flying cannot take off nor be at the airport.

--- specs/plane.spec.js
+++ specs/plane.spec.js
@@ -1,18 +1,28 @@
-const { assertEquals } = require('./testing-framework');
-const plane = require('../src/plane');
+const { assertEquals } = require("./testing-framework");
+const plane = require("../src/plane");
+const airport = require("../src/airport");
 
 let actual, result, expected;
-//Test 1
+//Test 6
 console.log(`==============`);
-console.log(`Test 1 Plane Spec`);
-console.log(` Testing if plane is at airport using isAtAirport(plane)`);
-
+console.log(`Test 6: if plane is at airport`);
 
 expected = true;
 actual = plane.isAtAirport();
 result = assertEquals(actual, expected);
 
-console.log(`Test 1 Plane Spec: Plane has landed at airport: ${result}`);
-console.log(`Test 1 Plane Spec:${result ? `PASS` : `FAIL`}`);
+console.log(`Test 6: Plane is at airport: ${result}`);
+console.log(`Test 6:${result ? `PASS` : `FAIL`}`);
 console.log(``);
 
+//Test 7
+console.log(`==============`);
+console.log(`Test 7: planes already flying cannot take off nor be at an airport`);
+
+expected = false;
+actual = plane.isPlaneFlying();
+result = assertEquals(actual, expected);
+
+console.log(`Test 7: Flying plane cannot take off nor be at an airport: ${result}`);
+console.log(`Test 7: ${result ? `PASS` : `FAIL`}`);
+console.log(``);
