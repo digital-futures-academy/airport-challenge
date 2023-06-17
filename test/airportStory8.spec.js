e9162921c1abd8edd1caba6dcc2a55185150f69a
KeenanMR <keenan.r.michael@gmail.com>
2023-06-17 16:49:47 +0100

    Added a second test  to User story 8
    This test already passed due to a check for a previous test so I am unsure if I should leave it in or remove it

--- test/airportStory8.spec.js
+++ test/airportStory8.spec.js
@@ -36,7 +36,37 @@ Test = () => {
 
 }
 
+Test2 = () => {
 
-//Test 2 Multiple airports
+    console.log(chalk.cyan(`User Story 8: The plane should be unable to land at a second airport `))
+    console.log(chalk.cyan(`User Story 8 Test 2: Try to land the same plane at 2 airports`))
+    console.log(chalk.cyan(`User Story 8 Pass Criteria: The plane should not land at a second airport`))
+    console.log(``);
+
+    //Arrange
+    plane = new Plane(`Plane`);
+    airport = new Airport(2);
+    airport2 = new Airport(2);
+    expected = 0
+
+    //Act
+    airport.landPlane(plane);
+    airport2.landPlane(plane);
+    actual = airport2.getPlanesAtAirport().length;
+
+    //Assert
+    result = assertStrictEqual(expected, actual);
+
+    console.log(chalk.cyan(`User Story 8 Test 2 Result: ${result ? chalk.green(`PASS`) : chalk.red(`FAIL`)}`))
+    console.log(chalk.red(`=====================================================================================================`))
+    console.log(``);
+
+    //Cleanup
+    input = undefined;
+    expected = undefined;
+
+}
+
 
 Test();
+Test2();
