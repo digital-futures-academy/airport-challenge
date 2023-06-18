4da420067d78269bd8c6c376d5706dbdb342ca42
Daniel Ayesoro <dayesoro@gmail.com>
2023-06-18 03:29:53 +0100

    wrote production code to pass User Story 1: Test 1

--- src/airport.js
+++ src/airport.js
@@ -1,1 +1,19 @@
+class Airport {
 
+  #airportPlane;
+  #planesAtAirport = [];
+
+  constructor(planeObject) {
+    this.#airportPlane = planeObject;
+  }
+
+  landPlane() {
+    this.#planesAtAirport.push(this.#airportPlane);
+  }
+
+  getPlanesLength() {
+    return this.#planesAtAirport.length;
+  }
+}
+
+module.exports = Airport; 
