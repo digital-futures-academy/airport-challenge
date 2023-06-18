b16ca0a7e0a4007ff6028af4b05feec80217ff21
zzhao30 <zzhao30@sheffield.ac.uk>
2023-06-19 01:08:00 +0800

    all files

--- specRunner.js
+++ specRunner.js
@@ -2,4 +2,6 @@ console.log('RUNNING ALL TESTS');
 console.log('=================');
 console.log('');
 
-//require('./spec/');
+require('./spec/airportSpec');
+require('./spec/planeSpec');
+require('./spec/weatherSpec');
