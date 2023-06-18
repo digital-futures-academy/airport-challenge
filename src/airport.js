40a105ef519b2589e4e7a1230c08fdbe5710c091
zzhao30 <zzhao30@sheffield.ac.uk>
2023-06-19 02:03:22 +0800

    airport funcs done

--- src/airport.js
+++ src/airport.js
@@ -1,4 +1,28 @@
 class Airport{
+    constructor(name, airportCapacity = 10){
+        this.name = name;
+        this.airportCapacity = airportCapacity;
+        this.currentPlanes = [];
+
+    }
+    isFull(){
+        return this.currentPlanes.length >= this.airportCapacity;
+    }
+
+    landPlane(plane){
+        this.currentPlanes.push(plane);
+    }
+
+    takeoffPlane(plane){
+        let newCurrentPlanes = [];
+        for (let i = 0; i < this.currentPlanes.length; i++) {
+            if (this.currentPlanes[i].planeId !== plane.planeId) {
+            newCurrentPlanes.push(this.currentPlanes[i]);
+            }
+        }
+        this.currentPlanes = newCurrentPlanes;
+
+    }
 
 
 
