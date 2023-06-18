30d6644f633fe3a97ea189c0fc580b5ebcf8bcd9
Aimable Mardel <73969781+aimardel@users.noreply.github.com>
2023-06-19 00:50:07 +0100

    Test 1a: Refactored code to pass test

--- src/instruction.js
+++ src/instruction.js
@@ -1,4 +1,14 @@
 class Instruction {
-
+    #code;
+    #airport;
+    #plane;
+    constructor(code, airport, plane) {
+        this.#code = code;
+        this.#airport = airport;
+        this.#plane = plane;
+    }
+    landPlane() {
+        this.#airport.landPlane(this.#plane);
+    }
 }
 export default Instruction;
