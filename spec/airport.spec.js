bec515a8c10be40ce0fa3d88c7c6da9fa658dbe9
Aimable Mardel <73969781+aimardel@users.noreply.github.com>
2023-06-19 02:04:45 +0100

    Moving tests more suited for the airport spec away from the instruction spec

--- spec/airport.spec.js
+++ spec/airport.spec.js
@@ -1,2 +1,26 @@
 import Airport from '../src/airport.js';
 
+describe('User Story 1 Test', () => {
+    let testAirport;
+
+    describe('Airport instantiation tests', () => {
+        const mockPlane = {
+            isLanded: () => true,
+        }
+        beforeEach(() => {
+            testAirport = new Airport("testAirport");
+        })
+        afterEach(() => {
+            testAirport = undefined;
+        })
+        it('1b. should call the plane\'s isLanded method', () => {
+            // Arrange -> testInstruction done in beforeEach
+            const mockPlaneSpy = spyOn(mockPlane, 'isLanded');
+            // Act
+            testAirport.landPlane();
+            // Assert
+            expect(mockPlaneSpy).toHaveBeenCalled();
+        })
+
+    })
+})
