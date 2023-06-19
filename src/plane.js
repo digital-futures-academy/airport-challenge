d6224bddb52e98fde3a7f3ba005d447da0052fd0
Aimable Mardel <73969781+aimardel@users.noreply.github.com>
2023-06-19 02:30:14 +0100

    Test 1c: Added the plane class

---
+++ src/plane.js
@@ -0,0 +1,11 @@
+class Plane {
+    flightNumber;
+    landed;
+    constructor(flightNumber) {
+        this.flightNumber = flightNumber;
+        this.landed = false;
+    }
+
+    isLanded() { }
+}
+export default Plane;
