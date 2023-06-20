68eb1c4e23548c669882338d575fb9447b0650ac
Oana <oana.demian@gmail.com>
2023-06-20 17:14:25 +0100

    fixes formatting errors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
-import Plane from "./Plane.js";
-import Airport from "./Airport.js";
-import Weather from "./Weather.js";
+import Plane from './Plane.js';
+import Airport from './Airport.js';
+import Weather from './Weather.js';
 
 let airport, plane1, plane2, plane3, weather, currentWeather;
 
@@ -20,10 +20,10 @@ console.log('\n3. Make an airport with id LHR, land a plane with id G-XLEE and a
 airport = new Airport('LHR');
 plane1 = new Plane('G-XLEE', 'landed');
 try {
-  airport.landPlane(plane1)
+  airport.landPlane(plane1);
 } catch (error) {
   console.log(error.message);
-};
+}
 
 console.log('\n4. Make an airport with id OTP, land three planes in the airport and report:');
 airport = new Airport('OTP');
@@ -43,7 +43,7 @@ plane2 = new Plane('YR-ATC', 'departed');
 airport.landPlane(plane1);
 console.log(`At the moment, maximum airport capacity is ${airport.maxAirportCapacity} and there are ${airport.landedPlanes.length} planes at the airport. Is airport full?`, airport.isAirportFull());
 airport.overrideAirportCapacityBy(-9);
-console.log(`We override maximum airport capacity to be ${airport.maxAirportCapacity}.`)
+console.log(`We override maximum airport capacity to be ${airport.maxAirportCapacity}.`);
 console.log(`At the moment, maximum airport capacity is ${airport.maxAirportCapacity} and there are ${airport.landedPlanes.length} planes at the airport. Is airport full?`, airport.isAirportFull());
 
 
@@ -54,7 +54,7 @@ plane2 = new Plane('YR-ATC', 'departed');
 airport.overrideAirportCapacityBy(-9);
 airport.landPlane(plane1);
 airport.landPlane(plane2);
-console.log(`Is ${plane2.aircraftId} at the airport? ${airport.isAirportFull()? `No, the airport is full so ${plane2.aircraftId} did not land` : `Yes, the airport is not full so ${plane2.aircraftId} landed`}.`)
+console.log(`Is ${plane2.aircraftId} at the airport? ${airport.isAirportFull()? `No, the airport is full so ${plane2.aircraftId} did not land` : `Yes, the airport is not full so ${plane2.aircraftId} landed`}.`);
 
 console.log('\n7. Make an airport with name LHR, land a plane with id G-XLEE if weather is clear and report');
 airport = new Airport('LHR');
@@ -63,10 +63,10 @@ weather = new Weather();
 currentWeather = weather.getCurrentWeather();
 console.log(`At present the weather is ${currentWeather}, therefore plane ${plane1.aircraftId} ${currentWeather === 'clear' ? 'can land' : 'cannot land'}`);
 try {
-  airport.landPlane(plane1, currentWeather)
+  airport.landPlane(plane1, currentWeather);
 } catch (error) {
   console.log(error.message);
-};
+}
 
 console.log('\n8. Plane takes off if weather is clear');
 airport = new Airport('LHR');
@@ -89,7 +89,7 @@ weather = new Weather();
 currentWeather = weather.getCurrentWeather();
 console.log(`At present the weather is ${currentWeather}, therefore plane ${plane1.aircraftId} ${currentWeather === 'clear' ? 'can take off' : 'cannot take off'}`);
 try {
-  airport.planeTakeOff('YR-ATC', currentWeather)
+  airport.planeTakeOff('YR-ATC', currentWeather);
 } catch (error) {
   console.log(error.message);
-};
+}
